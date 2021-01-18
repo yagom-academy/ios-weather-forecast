ffd384da9eea74099d14adce0188b1e2cc58276b
wonhee <wonhee010@gmail.com>
2021-01-18 16:19:28 +0900

    feat: add CurrentWeather

--- WeatherForecast/WeatherForecast/Item/CurrentWeather.swift
+++ WeatherForecast/WeatherForecast/Item/CurrentWeather.swift
@@ -9,26 +9,12 @@ import Foundation
 
 struct CurrentWeather: Codable {
     let addressName: String
-    struct Weather: Codable {
-        let iconName: String
-        
-        enum CodingKeys: String, CodingKey {
-            case iconName = "icon"
-        }
-    }
-    struct Main: Codable {
-        let temperature: Double
-        let minTemperature: Double
-        let maxTemperature: Double
-        
-        enum CodingKeys: String, CodingKey {
-            case temperature = "temp"
-            case minTemperature = "temp_min"
-            case maxTemperature = "temp_max"
-        }
-    }
+    let temperatureInformation: Temperature
+    let weatherInformation: WeatherInformation
     
     enum CodingKeys: String, CodingKey {
         case addressName = "name"
+        case temperatureInformation = "main"
+        case weatherInformation = "weather"
     }
 }
