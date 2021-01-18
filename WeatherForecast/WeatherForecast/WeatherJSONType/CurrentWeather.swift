5da8a4c5f0eccc9147505bbf41bfa0be57d73e77
zdodev <iver0945@gmail.com>
2021-01-18 21:15:24 +0900

    feat: CurrentWeather 타입 정의

---
+++ WeatherForecast/WeatherForecast/WeatherJSONType/CurrentWeather.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentWeather.swift
+//  WeatherForecast
+//
+//  Created by Zero DotOne on 2021/01/18.
+//
+
+import Foundation
+
+struct CurrentWeather: Codable {
+    let coord: Coord
+    let weather: [Weather]
+    let main: Main
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone: Int
+    let id: Int
+    let name: String
+}
