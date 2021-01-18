087e04951e0c06b8d314b033886a10beabfa6b22
yeon <jryoun0404@gmail.com>
2021-01-18 23:39:30 +0900

    Feat: Forecast 타입 생성

---
+++ WeatherForecast/WeatherForecast/Forecast.swift
@@ -0,0 +1,20 @@
+//
+//  Forecast.swift
+//  WeatherForecast
+//
+//  Created by Yeon on 2021/01/18.
+//
+
+import Foundation
+
+struct Forecast: Codable {
+    let dateTime: Date
+    let icon: WeatherIcon
+    let temperature: Temperature
+    
+    enum CodingKeys: String, CodingKey {
+        case dateTime = "dt_txt"
+        case icon = "weather"
+        case temperature = "main"
+    }
+}
