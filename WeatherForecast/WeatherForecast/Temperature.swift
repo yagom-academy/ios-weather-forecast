2e7037cbaeb04eeacf15ea18a3624f71a71bd60a
yeon <jryoun0404@gmail.com>
2021-01-18 23:39:03 +0900

    Feat: Temperature 타입 생성

---
+++ WeatherForecast/WeatherForecast/Temperature.swift
@@ -0,0 +1,20 @@
+//
+//  Temperature.swift
+//  WeatherForecast
+//
+//  Created by Yeon on 2021/01/18.
+//
+
+import Foundation
+
+struct Temperature: Codable {
+    let average: Double
+    let minimun: Double
+    let maximum: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case average = "temp"
+        case minimun = "temp_min"
+        case maximum = "temp_max"
+    }
+}
