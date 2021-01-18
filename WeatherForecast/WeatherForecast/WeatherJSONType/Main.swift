e1abd7be8c304329f4f624f9ce6bbeaf2457df67
zdodev <iver0945@gmail.com>
2021-01-18 21:38:26 +0900

    refactor: Main 타입 CodingKey 설정.

--- WeatherForecast/WeatherForecast/WeatherJSONType/Main.swift
+++ WeatherForecast/WeatherForecast/WeatherJSONType/Main.swift
@@ -9,9 +9,18 @@ import Foundation
 
 struct Main: Codable {
     let temp: Double
-    let feels_like: Double
+    let feelsLike: Double
     let pressure: Int
     let humidity: Int
-    let temp_min: Double
-    let temp_max: Double
+    let tempMin: Double
+    let tempMax: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case pressure
+        case humidity
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+    }
 }
