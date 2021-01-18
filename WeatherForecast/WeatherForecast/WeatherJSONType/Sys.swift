0b23d964a4a54fdf96c7831a99a9373808e618c0
zdodev <iver0945@gmail.com>
2021-01-18 21:19:08 +0900

    feat: Sys 타입 정의

---
+++ WeatherForecast/WeatherForecast/WeatherJSONType/Sys.swift
@@ -0,0 +1,14 @@
+//
+//  Sys.swift
+//  WeatherForecast
+//
+//  Created by Zero DotOne on 2021/01/18.
+//
+
+import Foundation
+
+struct Sys: Codable {
+    let country: String
+    let sunrise: Int
+    let sunset: Int
+}
