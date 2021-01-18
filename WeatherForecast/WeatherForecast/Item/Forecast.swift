6a700a7acfc52c343e7f0d319674a6161b006909
wonhee <wonhee010@gmail.com>
2021-01-18 16:23:33 +0900

    feat: edit Forecast property Type

--- WeatherForecast/WeatherForecast/Item/Forecast.swift
+++ WeatherForecast/WeatherForecast/Item/Forecast.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Forecast: Codable {
     let itemCount: Int
     let list: [ForecastItem]
-    let city: [CityInformation]
+    let city: CityInformation
     
     enum CodingKeys: String, CodingKey {
         case itemCount = "cnt"
