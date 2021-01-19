986d72aa77e99b4cf84f2131a1472269c5042830
GREENOVER <humains29@gmail.com>
2021-01-19 17:41:34 +0900

    feat: Temperature 구조체 섭씨 온도 변환 연산 프로퍼티 추가

--- WeatherForecast/WeatherForecast/Model/Temperature.swift
+++ WeatherForecast/WeatherForecast/Model/Temperature.swift
@@ -5,6 +5,18 @@ struct Temperature: Codable {
     let minimum: Double
     let maximum: Double
     
+    var celsiusAverage: Double {
+        return UnitTemperature.celsius.converter.value(fromBaseUnitValue: self.average)
+    }
+    
+    var celsiusMinimum: Double {
+        return UnitTemperature.celsius.converter.value(fromBaseUnitValue: self.minimum)
+    }
+    
+    var celsiusMaximum: Double {
+        return UnitTemperature.celsius.converter.value(fromBaseUnitValue: self.maximum)
+    }
+    
     enum CodingKeys: String, CodingKey {
         case average = "temp"
         case minimum = "temp_min"
