b6086dedcc5f081b8fff9b6e12b73377f82907b6
sole <subuchappyv2@gmail.com>
2021-01-19 15:23:42 +0900

    feat: Wind type

---
+++ WeatherForecast/WeatherForecast/ResponseData/Wind.swift
@@ -0,0 +1,14 @@
+//
+//  Wind.swift
+//  WeatherForecast
+//
+//  Created by sole on 2021/01/19.
+//
+
+import Foundation
+
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
