24bb3890e606e19ad0f0886a9811a696e5323b5b
yeon <jryoun0404@gmail.com>
2021-01-19 13:26:56 +0900

    Refactor: Temperature 타입에 프로퍼티 추가

--- WeatherForecast/WeatherForecast/Model/Temperature.swift
+++ WeatherForecast/WeatherForecast/Model/Temperature.swift
@@ -11,10 +11,15 @@ struct Temperature: Codable {
     let average: Double
     let minimun: Double
     let maximum: Double
+    let humanFeel: Double
+    let pressure: Double
+    let humidity: Double
     
     enum CodingKeys: String, CodingKey {
         case average = "temp"
         case minimun = "temp_min"
         case maximum = "temp_max"
+        case humanFeel = "feels_like"
+        case pressure, humidity 
     }
 }
