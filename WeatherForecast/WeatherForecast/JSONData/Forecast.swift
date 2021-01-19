6426c076f13bc785040e53169c0b5c79f4494fa0
odong-Tree <ss12357@naver.com>
2021-01-19 14:14:10 +0900

    feat: Forecast.swift에 Forecast 타입 구현

---
+++ WeatherForecast/WeatherForecast/JSONData/Forecast.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+struct Forecast: Codable {
+    let dateTime: Date
+    let weather: Weather
+    let temperature: Temperature
+    
+    enum CodingKeys: String, CodingKey {
+        case dateTime = "dt_txt"
+        case weather
+        case temperature = "main"
+    }
+}
