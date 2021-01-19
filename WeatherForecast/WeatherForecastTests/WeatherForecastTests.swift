9a934a7dc7988b99bae88069485df4f11315df4b
SM <hcooch2ch3@gmail.com>
2021-01-19 21:35:30 +0900

    feat: CurrentWeatherResponse struct decode test 코드 구현

--- WeatherForecast/WeatherForecastTests/WeatherForecastTests.swift
+++ WeatherForecast/WeatherForecastTests/WeatherForecastTests.swift
@@ -8,24 +8,27 @@ import XCTest
 @testable import WeatherForecast
 
 class WeatherForecastTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    let decoder = JSONDecoder()
+    let bundle = Bundle(for: WeatherForecastTests.self)
+    
+    func testDecodeCurrentWeatherResponseJSON() {
+        let fileName = "CurrentWeatherResponse"
+        
+        guard let url = bundle.url(forResource: fileName, withExtension: "json") else {
+            XCTFail("URL Error")
+            return
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            XCTFail("Data Error")
+            return
+        }
+        
+        do {
+            let result = try decoder.decode(CurrentWeatherResponse.self, from: data)
+            print(result)
+        } catch {
+            XCTFail(error.localizedDescription)
         }
     }
 
