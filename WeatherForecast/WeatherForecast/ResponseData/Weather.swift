440e8908101e127b0744866aaf7bdec19d332024
sole <subuchappyv2@gmail.com>
2021-01-19 15:17:25 +0900

    feat: Weather type

---
+++ WeatherForecast/WeatherForecast/ResponseData/Weather.swift
@@ -0,0 +1,15 @@
+//
+//  Weather.swift
+//  WeatherForecast
+//
+//  Created by sole on 2021/01/19.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon : String 
+}
