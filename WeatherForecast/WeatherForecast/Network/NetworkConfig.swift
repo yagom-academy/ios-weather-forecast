7aa027fe6585e944e60210f54549bbe84d6e6b30
wonhee <wonhee010@gmail.com>
2021-01-20 23:08:08 +0900

    feat: add networkconfig

--- WeatherForecast/WeatherForecast/Network/NetworkConfig.swift
+++ WeatherForecast/WeatherForecast/Network/NetworkConfig.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct NetworkConfig {
+    private static let appId = "dfae32caf228b6ced799aaadfd1babec"
+    private static let queryFormat = "lat=%d&lon=%d&appid=%@"
+    private static let currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?"
+    private static let fiveDaysForecastAPI = "https://api.openweathermap.org/data/2.5/forecast?"
+    
+    func makeWeatherUrlString(type: WeatherAPI, latitude: Double, longitude: Double) -> String {
+        var urlString: String
+        switch type {
+        case .current:
+            urlString = NetworkConfig.currentWeatherAPI
+        case .fiveDaysForecast:
+            urlString = NetworkConfig.fiveDaysForecastAPI
+        }
+        let queryString = String(format: NetworkConfig.queryFormat, latitude, longitude, NetworkConfig.appId)
+        urlString.append(queryString)
+        
+        return urlString
+    }
+}
