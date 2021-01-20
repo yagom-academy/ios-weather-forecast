741904517ded46ec279e39937b477235f3517ca2
wonhee <wonhee010@gmail.com>
2021-01-21 01:59:37 +0900

    feat: add common network func

--- WeatherForecast/WeatherForecast/Network/WeatherNetwork.swift
+++ WeatherForecast/WeatherForecast/Network/WeatherNetwork.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class WeatherNetwork {
+    static let shared = WeatherNetwork()
+    private init() {}
+    
+    func loadData(from url: URL, completion: @escaping ((Result<Data?, WeatherForcastError>) -> Void)) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if error != nil {
+                completion(.failure(.network))
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                return completion(.failure(.network))
+            }
+            if let data = data {
+                completion(.success(data))
+            }
+        }.resume()
+    }
+}
