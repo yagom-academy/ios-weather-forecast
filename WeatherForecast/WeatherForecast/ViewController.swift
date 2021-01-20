794a51aafae7ac31e2f8ac61b19d425c4e5a8298
GREENOVER <humains29@gmail.com>
2021-01-20 16:08:08 +0900

    feat: API 키값과 위,경도 변수 생성

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -1,16 +1,13 @@
-//
-//  WeatherForecast - ViewController.swift
-//  Created by yagom. 
-//  Copyright © yagom. All rights reserved.
-// 
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let apiKey: String = "02337d7f6423f1596383c1797c318936"
+    var latitude: Double = 37.68382
+    var longitude: Double = 126.742401
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
