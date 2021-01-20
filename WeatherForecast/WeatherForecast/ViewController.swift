42d181063b7406c8f75a572466a080eae057219a
sole <subuchappyv2@gmail.com>
2021-01-20 15:36:50 +0900

    feat: 사용자위치정보가져오기

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -5,14 +5,33 @@
 // 
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    var locationManager: CLLocationManager!
+    var latitude: Double?
+    var longitude: Double?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUserCoordinate()
     }
+}
 
-
+extension ViewController: CLLocationManagerDelegate {
+    func setUserCoordinate() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        //포그라운드일때 위치 추적 권한 요청
+        locationManager.requestWhenInUseAuthorization()
+        //베터라에 맞게 권장되는 최적의 정확도
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //위치업데이트
+        locationManager.startUpdatingLocation()
+        //위경도 가져오기
+        let coor = locationManager.location?.coordinate
+        latitude = coor?.latitude
+        longitude = coor?.longitude
+    }
 }
 
