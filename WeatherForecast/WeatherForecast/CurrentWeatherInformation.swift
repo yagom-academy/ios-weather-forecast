1d998026a6fba9eb48f62184e1fb889a0548a4e7
innieminnie <rkddlsl1001@gmail.com>
2021-01-20 15:38:28 +0900

    feat: 현재날씨확인 기능을 위한 타입 구현
    CurrentWeatherInformation(현재날씨정보)타입과 해당 타입을 구성하는 GeographicCoordinate(지리좌표계), Weather(날씨), CountryInformation(나라정보), Temperature(온도) 구현

---
+++ WeatherForecast/WeatherForecast/CurrentWeatherInformation.swift
@@ -0,0 +1,63 @@
+
+import Foundation
+
+struct GeographicCoordinate: Decodable {
+    var latitude: Double
+    var longitude: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case latitude = "lat"
+        case longitude = "lon"
+    }
+}
+
+struct Weather: Decodable {
+    var id: Int
+    var main: String
+    var description: String
+    var iconId: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, main, description
+        case iconId = "icon"
+    }
+}
+
+struct CountryInformation: Decodable {
+    var code: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case code = "country"
+    }
+}
+
+struct Temperature: Decodable {
+    var currentTemperature: Double
+    var minimumTemperature: Double
+    var maximumTemperature: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case currentTemperature = "temp"
+        case minimumTemperature = "temp_min"
+        case maximumTemperature = "temp_max"
+    }
+}
+
+
+struct CurrentWeatherInformation: Decodable {
+    var geographicCoordinate: GeographicCoordinate
+    var dataTimeCalculation: Double
+    var cityName: String
+    var weather: [Weather]
+    var countryInformation: CountryInformation
+    var temperature: Temperature
+    
+    private enum CodingKeys: String, CodingKey {
+        case geographicCoordinate = "coord"
+        case dataTimeCalculation = "dt"
+        case cityName = "name"
+        case weather
+        case countryInformation = "sys"
+        case temperature = "main"
+    }
+}
