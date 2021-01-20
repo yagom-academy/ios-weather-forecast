281ad36eec4e199147293597cff39c8a800dc606
wonhee <wonhee010@gmail.com>
2021-01-20 23:28:26 +0900

    feat: add error case

--- WeatherForecast/WeatherForecast/Error/WeatherForcastError.swift
+++ WeatherForecast/WeatherForecast/Error/WeatherForcastError.swift
@@ -11,6 +11,7 @@ enum WeatherForcastError: Error {
     case getCoordinate
     case openSettings
     case convertURL
+    case convertWeatherData
     case unknown
 }
 
@@ -23,6 +24,8 @@ extension WeatherForcastError: LocalizedError {
             return "설정 화면 이동에 실패했습니다.\n잠시 후 다시 시도해 주세요."
         case .convertURL:
             return "URL 변환에 실패했습니다.\n잠시 후 다시 시도해 주세요."
+        case .convertWeatherData:
+            return "데이터를 변환하는 중 오류가 발생했습니다.\n잠시 후 다시 시도해 주세요."
         case .unknown:
             return "알 수 없는 오류가 발생했습니다.\n잠시 후 다시 시도해 주세요."
         }
