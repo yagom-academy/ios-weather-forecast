914714be632d1b4516d3749cfed79e994dd7ed49
taetae <taetae.dev@gmail.com>
2021-01-20 17:47:16 +0900

    feat: API를 통해 현재날씨, 5일 예보에 대한 JSON 받아오는 기능 구현

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -5,24 +5,46 @@
 // 
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    var currentWeather: ForecastFiveDays?
+final class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    private var currentWeather: CurrentWeather?
+    private var forecastFiveDays: ForecastFiveDays?
     
     private func decodeCurrentWeaterFromAPI() {
         let session = URLSession(configuration: .default)
-        guard let url:URL = URL(string: "https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=125&appid=4119f1d1ea30af76104279475caf11c7") else {
+        guard let url:URL = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=36&lon=128&appid=bdc8daed0ec51c18dfc0d8b9c84bb17c") else {
             return
         }
-
+        
         let dataTask = session.dataTask(with: url) { data,_,error  in
             guard let data = data else {
                 return
             }
-
+            
             do {
-                self.currentWeather = try JSONDecoder().decode(ForecastFiveDays.self, from: data)
+                self.currentWeather = try JSONDecoder().decode(CurrentWeather.self, from: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func decodeForecastFiveDaysFromAPI() {
+        let session = URLSession(configuration: .default)
+        guard let url:URL = URL(string: "https://api.openweathermap.org/data/2.5/forecast?lat=36&lon=128&appid=bdc8daed0ec51c18dfc0d8b9c84bb17c") else {
+            return
+        }
+        
+        let dataTask = session.dataTask(with: url) { data,_,error  in
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                self.forecastFiveDays = try JSONDecoder().decode(ForecastFiveDays.self, from: data)
             } catch {
                 print(error.localizedDescription)
             }
@@ -34,12 +56,15 @@ class ViewController: UIViewController {
         dump(currentWeather)
     }
     
+    @IBAction func printForecastFiveDays() {
+        dump(forecastFiveDays)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         decodeCurrentWeaterFromAPI()
-
+        decodeForecastFiveDaysFromAPI()
     }
-
 }
 
 
