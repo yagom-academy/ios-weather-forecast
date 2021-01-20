98b3a4dc6148d8299a625da2d845949567043a0e
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-20 19:57:27 +0900

    Feat #11 - WeatherForecast 디코드 데이터 테스트케이스 구현

--- WeatherForecast/WeatherForecastTests/WeatherForecastTests.swift
+++ WeatherForecast/WeatherForecastTests/WeatherForecastTests.swift
@@ -51,7 +51,47 @@ class WeatherForecastTests: XCTestCase {
             XCTAssertEqual(decodedData.cityID, 420006353)
             XCTAssertEqual(decodedData.cityName, "Mountain View")
         } catch {
-            XCTFail("\(error)")
+            XCTFail("\(error.localizedDescription)")
+        }
+    }
+    
+    func testDecodeResponseDataOfWeatherForecastAPI() {
+        // Given-When-Then (준비-실행-검증)
+        // Given
+        guard let dataAsset = NSDataAsset(name: "ExampleResponseOfWeatherForecast") else {
+            XCTFail("Failed to load dataAsset")
+            return
+        }
+        let jsonDecoder = JSONDecoder()
+        let decodedData: WeatherForecast
+            
+        do {
+            // When
+            decodedData = try jsonDecoder.decode(WeatherForecast.self, from: dataAsset.data)
+            
+            // Then
+            XCTAssertEqual(decodedData.count, 3)
+            for i in 0..<3 {
+                XCTAssertEqual(decodedData.list[i].utc, 1596564000)
+                XCTAssertEqual(decodedData.list[i].temperature.current, 293.55)
+                XCTAssertEqual(decodedData.list[i].temperature.humanFeels, 293.13)
+                XCTAssertEqual(decodedData.list[i].temperature.minimum, 293.55)
+                XCTAssertEqual(decodedData.list[i].temperature.maximum, 294.05)
+                XCTAssertEqual(decodedData.list[i].temperature.atmosphericPressure, 1013)
+                XCTAssertEqual(decodedData.list[i].temperature.humidity, 84)
+                XCTAssertEqual(decodedData.list[i].weather.conditionID, 500)
+                
+                XCTAssertEqual(decodedData.list[i].weather.group, "Rain")
+                XCTAssertEqual(decodedData.list[i].weather.description, "light rain")
+                XCTAssertEqual(decodedData.list[i].weather.iconID, "10d")
+                XCTAssertEqual(decodedData.list[i].dateTimeString, "2020-08-04 18:00:00")
+            }
+            XCTAssertEqual(decodedData.city.id, 2643743)
+            XCTAssertEqual(decodedData.city.name, "London")
+            XCTAssertEqual(decodedData.city.coordinate.latitude, 51.5073)
+            XCTAssertEqual(decodedData.city.coordinate.longitude, -0.1277)
+        } catch {
+            XCTFail("\(error.localizedDescription)")
         }
     }
 }
