cdbfe8d92db1fdaa81837e7c9d603140cb7aafe7
innieminnie <rkddlsl1001@gmail.com>
2021-01-20 17:08:30 +0900

    feat: 5일 예보 기능을 위한 타입 구현
    FiveDaysForecastingInformation타입과 해당 타입 구현을 위한 City(도시정보), ForecastingInformation(각각의 예보 정보) 타입 구현

--- WeatherForecast/WeatherForecast/CurrentWeatherInformation.swift
+++ WeatherForecast/WeatherForecast/CurrentWeatherInformation.swift
@@ -50,3 +50,36 @@ struct CurrentWeatherInformation: Decodable {
         case temperature = "main"
     }
 }
+
+struct City: Decodable {
+    var name: String
+    var geographicCoordinate: GeographicCoordinate
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case geographicCoordinate = "coord"
+    }
+}
+
+struct ForecastingInformation: Decodable {
+    var dateTimeCalculation: Double
+    var temperature: Temperature
+    var weather: [Weather]
+    
+    private enum CodingKeys: String, CodingKey {
+        case dateTimeCalculation = "dt"
+        case temperature = "main"
+        case weather
+    }
+}
+
+struct FiveDaysForecastingInformation: Decodable {
+    var forecastingList: [ForecastingInformation]
+    var city: City
+    
+    private enum CodingKeys: String, CodingKey {
+        case forecastingList = "list"
+        case city
+    }
+}
+
