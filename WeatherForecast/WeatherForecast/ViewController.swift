9ce87f72d35431b77acf05e5a6a51b84c495d786
wonhee <wonhee010@gmail.com>
2021-01-20 22:01:12 +0900

    feat: add CLLocationManager

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -5,13 +5,58 @@
 // 
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpLocationManager()
     }
-
-
+    
+    private func setUpData(coordinate: Coordinate) {
+        
+    }
 }
 
+// MARK: - CLLocationManager Delegate
+extension ViewController: CLLocationManagerDelegate {
+    private func setUpLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationPermission()
+    }
+    
+    func checkLocationPermission() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            searchCoordinate()
+        case .denied:
+            // TODO: handle don't have permission -> move settings UI
+            break
+        default:
+            return
+        }
+    }
+    
+    private func searchCoordinate() {
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let coordinate = locationManager.location?.coordinate else {
+            return self.showError(WeatherForcastError.getCoordinate, handler: nil)
+        }
+        setUpData(coordinate: Coordinate(latitude: coordinate.latitude, longitudh: coordinate.longitude))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        return self.showError(error, handler: nil)
+    }
+}
