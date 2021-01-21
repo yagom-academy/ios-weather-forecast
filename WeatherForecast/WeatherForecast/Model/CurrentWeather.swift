506e38aea353dd8e89238a0aa4e8b8b45c32d64e
yeon <jryoun0404@gmail.com>
2021-01-21 13:39:04 +0900

    Refactor: 실제로 필요한 Model들만 남기고 제거

--- WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
+++ WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
@@ -8,26 +8,13 @@
 import Foundation
 
 struct CurrentWeather: Codable {
-    let coordinate: Coordinate
     let icon: [WeatherIcon]
-    let base: String
     let temperature: Temperature
-    let visibility: Double
-    let wind: Wind
-    let clouds: Clouds
-    let timeOfDataCalculation: Date
-    let sys: Sys
-    let timezone: Double
-    let id: Double
     let cityName: String
-    let cod: Int
     
     enum CodingKeys: String, CodingKey {
-        case coordinate = "coord"
         case icon = "weather"
         case temperature = "main"
-        case timeOfDataCalculation = "dt"
         case cityName = "name"
-        case base, visibility, wind, clouds, sys, timezone, id, cod
     }
 }
--- WeatherForecast/WeatherForecast/Model/WeatherIcon.swift
+++ WeatherForecast/WeatherForecast/Model/WeatherIcon.swift
@@ -9,12 +9,8 @@ import Foundation
 
 struct WeatherIcon: Codable {
     let name: String
-    let main: String
-    let description: String
-    let id: Int
     
     enum CodingKeys: String, CodingKey {
         case name = "icon"
-        case main, description, id
     }
 }
