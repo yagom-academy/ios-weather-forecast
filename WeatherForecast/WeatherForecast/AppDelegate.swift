9be08019a5d1312d1d0611cb321936ecbca857af
yeon <jryoun0404@gmail.com>
2021-01-21 13:33:54 +0900

    Feat: CLLocationManager() 객체를 appDelegate에 생성해주고, 앱이 실행될 때 사용자에게 권한을 요청해준다.

--- WeatherForecast/WeatherForecast/AppDelegate.swift
+++ WeatherForecast/WeatherForecast/AppDelegate.swift
@@ -5,14 +5,16 @@
 // 
 
 import UIKit
+import CoreLocation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let locationManager = CLLocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        locationManager.requestWhenInUseAuthorization()
         return true
     }
 
