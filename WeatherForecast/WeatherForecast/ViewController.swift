290544bbf72b88aeca90caffb790e5600cd7220f
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-22 03:19:47 +0900

    Feat #14 - CLLocationManager로 현재 위치 정보 가져오기 시작

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -6,14 +6,31 @@
 // 
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    // MARK: - Properties
+    private lazy var locationManager: CLLocationManager = {
+        let locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        return locationManager
+    }()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.startUpdatingLocation()
     }
-
-
 }
 
+// MARK: - CLLocationManagerDelegate Methods
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+}
