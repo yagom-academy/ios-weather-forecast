5f25752c24aec1711020ffdd7f2b195580f02a21
wonhee <wonhee010@gmail.com>
2021-01-21 11:26:35 +0900

    feat: add ForecastModel to handle data

--- WeatherForecast/WeatherForecast/Model/ForecastModel.swift
+++ WeatherForecast/WeatherForecast/Model/ForecastModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class ForecastModel {
+    static let shared = ForecastModel()
+    private init() {}
+    
+    private(set) var item: FiveDaysForecast? = nil
+    
+    func fetchData(with coordinate: Coordinate, _ callback: @escaping (_ item: FiveDaysForecast?) -> Void) {
+        let urlString = NetworkConfig.makeWeatherUrlString(type: .fiveDaysForecast, coordinate: coordinate)
+        guard let url = URL(string: urlString) else {
+             return
+        }
+        WeatherNetwork.loadData(from: url, with: FiveDaysForecast.self) { result in
+            switch result {
+            case .failure:
+                callback(nil)
+            case .success(let data):
+                callback(data)
+            }
+        }
+    }
+}
