90f4917730ec4998b16fc616ebad428a429af6da
wonhee <wonhee010@gmail.com>
2021-01-22 21:17:41 +0900

    feat: add coordinate convert to address string

--- WeatherForecast/WeatherForecast/Model/WeatherModel.swift
+++ WeatherForecast/WeatherForecast/Model/WeatherModel.swift
@@ -6,12 +6,14 @@
 //
 
 import Foundation
+import CoreLocation
 
 class WeatherModel {
     static let shared = WeatherModel()
     private init() {}
     
     private(set) var item: CurrentWeather? = nil
+    private(set) var address: String? = nil
     
     func fetchData(with coordinate: Coordinate, _ callback: @escaping (_ item: CurrentWeather?) -> Void) {
         let urlString = NetworkConfig.makeWeatherUrlString(type: .current, coordinate: coordinate)
@@ -24,8 +26,34 @@ class WeatherModel {
                 callback(nil)
             case .success(let data):
                 self.item = data
+                self.coordinateToAddressString(data?.coordinate) { placemark in
+                    guard let city = placemark?.administrativeArea,
+                          let road = placemark?.thoroughfare else {
+                        self.address = nil
+                        return callback(data)
+                    }
+                    self.address = "\(city) \(road)"
+                }
                 callback(data)
             }
+        }
+    }
+    
+    func coordinateToAddressString(_ coordinate: Coordinate?, completionHandler: @escaping (CLPlacemark?)
+                    -> Void ) {
+        if let currentCoordinate = coordinate {
+            let geocoder = CLGeocoder()
+            let location = CLLocation(latitude: currentCoordinate.latitude, longitude: currentCoordinate.longitude)
+            geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+                if error == nil {
+                    let firstLoaction = placemarks?[0]
+                    completionHandler(firstLoaction)
+                } else {
+                    completionHandler(nil)
+                }
+            }
+        } else {
+            completionHandler(nil)
         }
     }
 }
