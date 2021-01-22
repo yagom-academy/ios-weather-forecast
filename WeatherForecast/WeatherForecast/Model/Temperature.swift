4ccae3f8fb69a0108636300bdbc9986d26ac150e
odong-Tree <ss12357@naver.com>
2021-01-23 01:24:38 +0900

    feat: 기온을 섭씨로 저장하는 연산프로퍼티 추가

--- WeatherForecast/WeatherForecast/Model/Temperature.swift
+++ WeatherForecast/WeatherForecast/Model/Temperature.swift
@@ -9,6 +9,21 @@ struct Temperature: Decodable {
     let pressure: Double
     let humidity: Double
     
+    var currentCelsius: Double {
+        let celsius = UnitTemperature.celsius.converter.value(fromBaseUnitValue: self.current)
+        return celsius
+    }
+    
+    var minCelsius: Double {
+        let celsius = UnitTemperature.celsius.converter.value(fromBaseUnitValue: self.min)
+        return celsius
+    }
+    
+    var maxCelsius: Double {
+        let celsius = UnitTemperature.celsius.converter.value(fromBaseUnitValue: self.max)
+        return celsius
+    }
+    
     enum CodingKeys: String, CodingKey {
         case current = "temp"
         case feelsLike = "feels_like"
