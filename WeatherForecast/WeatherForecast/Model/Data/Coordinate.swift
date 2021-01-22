92ab5d9d5883c1133af82cb87dd808817ccde537
wonhee <wonhee010@gmail.com>
2021-01-22 21:17:07 +0900

    feat: add Decodable

--- WeatherForecast/WeatherForecast/Model/Data/Coordinate.swift
+++ WeatherForecast/WeatherForecast/Model/Data/Coordinate.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-struct Coordinate {
+struct Coordinate: Decodable {
     let latitude: Double
     let longitude: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case latitude = "lat"
+        case longitude = "lon"
+    }
 }
