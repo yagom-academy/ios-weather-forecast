ef993b54a6eff2cf84678c01b942b4e016d1e5e2
wonhee <wonhee010@gmail.com>
2021-01-22 21:33:02 +0900

    feat: add init func

--- WeatherForecast/WeatherForecast/Cell/ForecastTableViewCell.swift
+++ WeatherForecast/WeatherForecast/Cell/ForecastTableViewCell.swift
@@ -10,9 +10,13 @@ import UIKit
 class ForecastTableViewCell: UITableViewCell {
     private let temperatureSign = "°"
 
+    // MARK: - init func
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
