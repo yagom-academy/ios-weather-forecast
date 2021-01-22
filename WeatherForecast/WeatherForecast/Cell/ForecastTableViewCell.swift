1d7227d8ad9211d476c6135d6972f9eedf26a068
wonhee <wonhee010@gmail.com>
2021-01-22 20:42:45 +0900

    feat: add init

--- WeatherForecast/WeatherForecast/Cell/ForecastTableViewCell.swift
+++ WeatherForecast/WeatherForecast/Cell/ForecastTableViewCell.swift
@@ -8,16 +8,11 @@
 import UIKit
 
 class ForecastTableViewCell: UITableViewCell {
+    private let temperatureSign = "°"
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
