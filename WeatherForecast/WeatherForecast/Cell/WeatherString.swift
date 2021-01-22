6665471b200e03fb9cd709067b3b0cb66e7ad4c2
wonhee <wonhee010@gmail.com>
2021-01-22 20:42:32 +0900

    feat: add String enum

---
+++ WeatherForecast/WeatherForecast/Cell/WeatherString.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherString.swift
+//  WeatherForecast
+//
+//  Created by Wonhee on 2021/01/22.
+//
+
+import Foundation
+
+enum WeatherString {
+    static let basic = "-"
+    static let temperatureSign = "°"
+    static let minMaxTemperatureFormat = "최저 %f° 최고 %f°"
+}
