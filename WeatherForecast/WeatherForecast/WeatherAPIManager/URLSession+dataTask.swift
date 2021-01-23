87241a98e30133eeeb728e63774892e8f64151d4
SM <hcooch2ch3@gmail.com>
2021-01-24 00:10:00 +0900

    feat: URLSession+dataTask에 Result를 입력으로 받는 handler가 파라미터인 dataTask 메소드 구현

--- WeatherForecast/WeatherForecast/WeatherAPIManager/URLSession+dataTask.swift
+++ WeatherForecast/WeatherForecast/WeatherAPIManager/URLSession+dataTask.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension URLSession {
+    func dataTask(with request: URLRequest, handler: @escaping (Result<Data, Error>) -> Void) -> URLSessionDataTask {
+        dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                handler(.failure(error))
+            } else {
+                handler(.success(data ?? Data()))
+            }
+        }
+    }
+}
