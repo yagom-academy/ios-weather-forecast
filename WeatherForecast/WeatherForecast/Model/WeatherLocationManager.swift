6cb7925db990458493fba81d10801f70887d0e83
SM <hcooch2ch3@gmail.com>
2021-01-23 22:27:22 +0900

    feat: WeatherLocationManager, WeatherLocationManagerDelegate swift파일 추가

---
+++ WeatherForecast/WeatherForecast/Model/WeatherLocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherLocationManager.swift
+//  WeatherForecast
+//
+//  Created by 임성민 on 2021/01/23.
+//
+
+import Foundation
---
+++ WeatherForecast/WeatherForecast/Model/WeatherLocationManagerDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherLocationManagerDelegate.swift
+//  WeatherForecast
+//
+//  Created by 임성민 on 2021/01/23.
+//
+
+import Foundation
