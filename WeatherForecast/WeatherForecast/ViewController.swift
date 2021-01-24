559babc7af0730644985fb91f98b6c5a80a67e19
yeon <jryoun0404@gmail.com>
2021-01-24 23:03:57 +0900

    Feat: TableView 코드로 작성

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -8,10 +8,34 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController {
-    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    private let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    private var currentWeather: CurrentWeather?
+    private var forecastList: ForecastList?
+    private let locationCoordinate: LocationCoordinate = LocationCoordinate()
+    private let weatherTableView: UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpLocationManager()
+        setUpTableView()
+    }
+    
+    private func setUpTableView() {
+        weatherTableView.dataSource = self
+        weatherTableView.delegate = self
+        self.weatherTableView.register(CurrentWeatherTableViewCell.self, forCellReuseIdentifier: "CurrentWeatherTableViewCell")
+        self.weatherTableView.register(ForecastTableViewCell.self, forCellReuseIdentifier: "ForecastTableViewCell")
+        
+        view.addSubview(weatherTableView)
+        let safeLayoutGuide = view.safeAreaLayoutGuide
+        weatherTableView.translatesAutoresizingMaskIntoConstraints = false
+        weatherTableView.allowsSelection = false
+        NSLayoutConstraint.activate([
+            weatherTableView.leadingAnchor.constraint(equalTo: safeLayoutGuide.leadingAnchor),
+            weatherTableView.trailingAnchor.constraint(equalTo: safeLayoutGuide.trailingAnchor),
+            weatherTableView.topAnchor.constraint(equalTo: safeLayoutGuide.topAnchor),
+            weatherTableView.bottomAnchor.constraint(equalTo: safeLayoutGuide.bottomAnchor)
+        ])
     }
     
     private func setUpLocationManager() {
@@ -36,8 +60,7 @@ extension ViewController: CLLocationManagerDelegate {
                     return errorHandling(error: .failGetData)
                 }
                 do {
-                    let forecastList: ForecastList = try JSONDecoder().decode(ForecastList.self, from: forecastData)
-                    print(forecastList)
+                    forecastList = try JSONDecoder().decode(ForecastList.self, from: forecastData)
                 } catch {
                     errorHandling(error: .failDecode)
                 }
@@ -53,8 +76,7 @@ extension ViewController: CLLocationManagerDelegate {
                     return errorHandling(error: .failGetData)
                 }
                 do {
-                    let currentWeather: CurrentWeather = try JSONDecoder().decode(CurrentWeather.self, from: currentData)
-                    print(currentWeather)
+                    currentWeather = try JSONDecoder().decode(CurrentWeather.self, from: currentData)
                 } catch {
                     errorHandling(error: .failDecode)
                 }
@@ -84,3 +106,44 @@ extension ViewController: CLLocationManagerDelegate {
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        default:
+            guard let numberOfRow = forecastList?.count else {
+                return 40
+            }
+            return numberOfRow
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let currentWeatherCell = weatherTableView.dequeueReusableCell(withIdentifier: "CurrentWeatherTableViewCell") as! CurrentWeatherTableViewCell
+            
+            return currentWeatherCell
+        default:
+            let forecastWeatherCell = weatherTableView.dequeueReusableCell(withIdentifier: "ForecastTableViewCell") as! ForecastTableViewCell
+            
+            return forecastWeatherCell
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 120
+        default:
+            return 50
+        }
+    }
+}
