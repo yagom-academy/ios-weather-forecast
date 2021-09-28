718685caf109f5f9781556035fe1dceb602c14fe
soll4u <dasollpark@gmail.com>
2021-09-28 17:53:29 +0900

    feat: 중첩 타입 8개 구현
    * WeatherInformation를 채택하는 타입에 선언 될 수 있는 중첩 타입이어서 해당 파일에 추가함

--- WeatherForecast/WeatherForecast/Model/WeatherInformation.swift
+++ WeatherForecast/WeatherForecast/Model/WeatherInformation.swift
@@ -11,4 +11,55 @@ protocol WeatherInformation {
     
 }
 
+struct Coordinate {
+    let lon: Double?
+    let lat: Double?
+}
 
+struct Weather: Codable {
+    let id: Int?
+    let main: String?
+    let description: String?
+    let icon: String?
+}
+
+struct MainInformation: Codable {
+    let temp: Double?
+    let feelsLike: Double?
+    let tempMin: Double?
+    let tempMax: Double?
+    let pressure: Int?
+    let humidity: Int?
+    let seaLevel: Int?
+    let grndLevel: Int?
+}
+
+struct Wind: Codable {
+    let speed: Double?
+    let deg: Int?
+    let gust: Double?
+}
+
+struct Clouds: Codable {
+    let all: Int?
+}
+
+struct Sys: Codable {
+    let type: Int?
+    let id: Int?
+    let message: Double?
+    let country: String?
+    let sunrise: Int?
+    let sunset: Int?
+    let pod: String?
+}
+
+struct Rain {
+    let oneHour: Int?
+    let threeHours: Int?
+}
+
+struct Snow {
+    let oneHour: Int?
+    let threeHours: Int?
+}
