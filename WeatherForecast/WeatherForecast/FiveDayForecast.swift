c52c6f7e29a4c542685433a99adf2bedf30147a2
ohsg0226 <ohsg0226@naver.com>
2021-09-28 18:25:28 +0900

    feat: FiveDayForeCast에 추가 타입 구현
    - List 타입 구현
      - Sys, Rain 타입 구현
    - City 타입 구현

--- WeatherForecast/WeatherForecast/FiveDayForecast.swift
+++ WeatherForecast/WeatherForecast/FiveDayForecast.swift
@@ -12,5 +12,51 @@ struct FiveDayForecast: Decodable {
     let message: Int
     let cnt: Int
     let list: [List]
-    let city: [City]
+    let city: City
+}
+
+struct City: Decodable {
+    let id: Int
+    let name: String
+    let coordinate: Coordinate
+    let country: String
+    let timezone: Int
+    let sunrise: Int
+    let sunset: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, country, timezone, sunrise, sunset
+        case coordinate = "coord"
+    }
+}
+
+struct List: Decodable {
+    let dt: Int
+    let main: Main
+    let weather: Weather
+    let cloud: Cloud
+    let wind: Wind
+    let visibility: Int
+    let pop: Double
+    let rain: Rain
+    let sys: Sys
+    let dtText: String
+    
+    struct Sys: Decodable {
+        let pod: String
+    }
+    
+    struct Rain: Decodable {
+        let threeHour: Double
+        
+        enum CodingKeys: String, CodingKey {
+            case threeHour = "3h"
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case dt, main, weather, wind, visibility, pop, rain, sys
+        case cloud = "clouds"
+        case dtText = "dt_txt"
+    }
 }
