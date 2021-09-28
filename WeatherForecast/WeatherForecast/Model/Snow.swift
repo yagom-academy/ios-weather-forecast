48336ad35c40021000ca3d49ae24ac9fd8925efc
shpairo711 <shapiro7@ajou.ac.kr>
2021-09-28 15:35:53 +0900

    feat: Snow 타입 구현

---
+++ WeatherForecast/WeatherForecast/Model/Snow.swift
@@ -0,0 +1,16 @@
+//
+//  Snow.swift
+//  WeatherForecast
+//
+//  Created by Kim Do hyung on 2021/09/28.
+//
+
+import Foundation
+
+struct Snow: Decodable {
+    let lastThreeHoursVolume: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case lastThreeHoursVolume = "3h"
+    }
+}
