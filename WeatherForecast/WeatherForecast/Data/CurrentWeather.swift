b9963cd12eb4c0f688194157a2520b5ddd695496
jaemuyeo <jemking94@gmail.com>
2021-09-28 23:15:16 +0900

    refactor: CurrentWeather 프로퍼티의 타입들을 중첩타입으로 변경

--- WeatherForecast/WeatherForecast/Data/CurrentWeather.swift
+++ WeatherForecast/WeatherForecast/Data/CurrentWeather.swift
@@ -21,50 +21,50 @@ struct CurrentWeather: Codable {
     let id: Int
     let name: String
     let cod: Int
-}
-
-struct Coord: Codable {
-    let lon: Double
-    let lat: Double
-}
-
-struct Weather: Codable {
-    let id: Int
-    let main: String
-    let description: String
-    let icon: String
-}
-
-struct Main: Codable {
-    let temp: Double
-    let feelsLike: Double
-    let tempMin: Double
-    let tempMax: Double
-    let pressure: Int
-    let humidity: Int
-    
-    enum CodingKeys: String, CodingKey {
-        case temp, pressure, humidity
-        case feelsLike = "feels_like"
-        case tempMin = "temp_min"
-        case tempMax = "temp_max"
+    struct Coord: Codable {
+        let lon: Double
+        let lat: Double
     }
-}
-
-struct Wind: Codable {
-    let speed: Double
-    let deg: Int
-}
-
-struct Clouds: Codable {
-    let all: Int
-}
-
-struct Sys: Codable {
-    let type: Int
-    let id: Int
-    let message: Double
-    let country: String
-    let sunrise: Int
-    let sunset: Int
+    
+    struct Weather: Codable {
+        let id: Int
+        let main: String
+        let description: String
+        let icon: String
+    }
+    
+    struct Main: Codable {
+        let temp: Double
+        let feelsLike: Double
+        let tempMin: Double
+        let tempMax: Double
+        let pressure: Int
+        let humidity: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case temp, pressure, humidity
+            case feelsLike = "feels_like"
+            case tempMin = "temp_min"
+            case tempMax = "temp_max"
+        }
+    }
+    
+    struct Wind: Codable {
+        let speed: Double
+        let deg: Int
+    }
+    
+    struct Clouds: Codable {
+        let all: Int
+    }
+    
+    struct Sys: Codable {
+        let type: Int
+        let id: Int
+        let message: Double
+        let country: String
+        let sunrise: Int
+        let sunset: Int
+    }
+    
 }
