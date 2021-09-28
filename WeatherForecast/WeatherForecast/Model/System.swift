ad9d19d1045b13e8b26abd2ebb2cc9b55b7e9be1
shpairo711 <shapiro7@ajou.ac.kr>
2021-09-28 15:18:37 +0900

    feat: System 타입 구현

---
+++ WeatherForecast/WeatherForecast/Model/System.swift
@@ -0,0 +1,23 @@
+//
+//  System.swift
+//  WeatherForecast
+//
+//  Created by Kim Do hyung on 2021/09/28.
+//
+
+import Foundation
+
+struct System: Decodable {
+    let type: Int
+    let id: Int
+    let message: Double
+    let country: String
+    let sunriseTime: TimeInterval
+    let sunsetTime: TimeInterval
+    
+    enum CodingKeys: String, CodingKey {
+        case type, id, country, message
+        case sunriseTime = "sunrise"
+        case sunsetTime = "sunset"
+    }
+}
