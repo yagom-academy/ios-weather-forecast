ae2cc79288e33b92c429ceb8440184a1e420d578
ictechgy <ictechgy@gmail.com>
2021-09-28 15:34:02 +0900

    feat: Rain타입 구현

---
+++ WeatherForecast/WeatherForecast/Model/Rain.swift
@@ -0,0 +1,16 @@
+//
+//  Rain.swift
+//  WeatherForecast
+//
+//  Created by JINHONG AN on 2021/09/28.
+//
+
+import Foundation
+
+struct Rain: Decodable {
+    let lastThreeHoursVolume: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case lastThreeHoursVolume = "3h"
+    }
+}
