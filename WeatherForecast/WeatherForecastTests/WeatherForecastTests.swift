1f66b75ee6538ce307faa2f6fb01e7bd3e5dfa93
Joey Hong <isjoeyful@gmail.com>
2021-09-28 22:04:33 +0900

    test: FiveDayWeather 타입을 parse(to:) 메서드로 파싱하는 테스트 구현

--- WeatherForecast/WeatherForecastTests/WeatherForecastTests.swift
+++ WeatherForecast/WeatherForecastTests/WeatherForecastTests.swift
@@ -29,4 +29,24 @@ class WeatherForecastTests: XCTestCase {
         }
     }
     
+    func test_FiveDayWeather파일을_parse메서드로디코딩했을때_List의count는5이다() {
+        //Given
+        let testFileName = "FiveDayWeather"
+        let testDataType = FiveDayWeather.self
+        guard let testData = NSDataAsset(name: testFileName)?.data else {
+            return XCTFail("\(testFileName)파일이 잘못되었습니다.")
+        }
+        let expectedValue = 40
+        
+        //When
+        let parsedResult = testData.parse(to: testDataType)
+        
+        //Then
+        switch parsedResult {
+        case .success(let decodedData):
+            XCTAssertEqual(decodedData.list.count, expectedValue)
+        default:
+            XCTFail("디코딩에 실패했습니다.")
+        }
+    }
 }
