8d6be2c155f6a81b9890cfeb289a94b89e050e6b
ictechgy <ictechgy@gmail.com>
2021-09-28 16:27:11 +0900

    refactor: System 타입 프로퍼티 변경
    - 값이 들어오는지 불확실하여 전체 프로퍼티를 옵셔널 타입으로 수정
    - partOfTheDay라는 값도 들어올 수 있어서 프로퍼티 추가

--- WeatherForecast/WeatherForecast/Model/System.swift
+++ WeatherForecast/WeatherForecast/Model/System.swift
@@ -8,16 +8,18 @@
 import Foundation
 
 struct System: Decodable {
-    let type: Int
-    let id: Int
-    let message: Double
-    let country: String
-    let sunriseTime: TimeInterval
-    let sunsetTime: TimeInterval
+    let type: Int?
+    let id: Int?
+    let message: Double?
+    let country: String?
+    let sunriseTime: TimeInterval?
+    let sunsetTime: TimeInterval?
+    let partOfTheDay: String?
     
     enum CodingKeys: String, CodingKey {
         case type, id, country, message
         case sunriseTime = "sunrise"
         case sunsetTime = "sunset"
+        case partOfTheDay = "pod"
     }
 }
