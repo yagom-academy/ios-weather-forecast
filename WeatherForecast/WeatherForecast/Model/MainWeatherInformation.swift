fdc79277122d9145a911e4bfde4d784ea8350e41
ictechgy <ictechgy@gmail.com>
2021-09-28 16:07:07 +0900

    refactor: MainWeatherInformation 타입에 프로퍼티 추가
    - 프로퍼티 네이밍을 직관적으로 수정
    - 프로퍼티에 값이 들어올지 불확실하므로 프로퍼티들의 타입을 옵셔널 타입으로 변경

--- WeatherForecast/WeatherForecast/Model/MainWeatherInformation.swift
+++ WeatherForecast/WeatherForecast/Model/MainWeatherInformation.swift
@@ -8,18 +8,24 @@
 import Foundation
 
 struct MainWeatherInformation: Decodable {
-    let temperature: Double
-    let feelsLike: Double
-    let minimumTemperature: Double
-    let maximumTemperature: Double
-    let pressure: Int
-    let humidity: Int
+    let temperature: Double?
+    let windChill: Double?
+    let minimumTemperature: Double?
+    let maximumTemperature: Double?
+    let pressure: Int?
+    let humidity: Int?
+    let seaLevelPressure: Int?
+    let groundLevelPressure: Int?
+    let freezingPoint: Double?
     
     enum CodingKeys: String, CodingKey {
         case temperature = "temp"
-        case feelsLike = "feels_like"
+        case windChill = "feels_like"
         case minimumTemperature = "temp_min"
         case maximumTemperature = "temp_max"
         case pressure, humidity
+        case seaLevelPressure = "sea_level"
+        case groundLevelPressure = "grnd_level"
+        case freezingPoint = "temp_kf"
     }
 }
