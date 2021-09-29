dc094d4062dfd86e2f70007f91a6156991713505
blanche37 <wkddbsghk16@gmail.com>
2021-09-29 14:33:55 +0900

    feat: API call이 가능한 url을 만들기위해 encode 메서드 구현

---
+++ WeatherForecast/WeatherForecast/Model/NetworkLayer/Networking/Encoding/URLParameterEncoder.swift
@@ -0,0 +1,27 @@
+//
+//  URLParameterEncoder.swift
+//  WeatherForecast
+//
+//  Created by yun on 2021/09/29.
+//
+
+import Foundation
+
+struct URLParameterEncoder: ParameterEncoding {
+    static func encode(urlRequest: inout URLRequest, with parameter: Parameters) throws {
+        guard let url = urlRequest.url else {
+            throw NetworkError.urlMissing
+        }
+        
+        if var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
+           !parameter.isEmpty {
+            urlComponents.queryItems = [URLQueryItem]()
+            for (key, value) in parameter {
+                let queryItem = URLQueryItem(name: key, value: "\(value)")
+                urlComponents.queryItems?.append(queryItem)
+            }
+            urlRequest.url = urlComponents.url
+        }
+    }
+}
+    
