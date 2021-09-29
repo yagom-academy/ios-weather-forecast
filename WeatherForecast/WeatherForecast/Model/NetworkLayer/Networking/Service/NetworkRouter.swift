a08dd09a46a41e0f52ac87fd7793e576bcc8e0de
blanche37 <wkddbsghk16@gmail.com>
2021-09-29 14:09:42 +0900

    feat: request메서드 구현

--- WeatherForecast/WeatherForecast/Model/NetworkLayer/Networking/Service/NetworkRouter.swift
+++ WeatherForecast/WeatherForecast/Model/NetworkLayer/Networking/Service/NetworkRouter.swift
@@ -12,19 +12,31 @@ typealias NetworkRouterCompletion = (_ data: Data?, _ response: URLResponse?, _ 
 protocol NetworkRouter {
     //associatedtype EndPointType = EndPoint
     
-    func request(_ route: EndPoint)
+    func request(_ route: EndPoint, _ session: URLSession)
     func cancel()
 }
 
 class Router: NetworkRouter {
-    func request(_ route: EndPoint) {
+    private var task: URLSessionDataTask?
+    
+    func request(_ route: EndPoint, _ session: URLSession) {
+        do {
+            let request = try self.buildRequest(from: route)
+            guard let url = request.url else {
+                return
+            }
+            task = session.dataTask(with: url)
+        } catch {
+            print(error)
+        }
+        self.task?.resume()
     }
     
     func cancel() {
     }
     
-    func buildRequest(url: EndPoint) -> URLRequest {
-        let request = URLRequest(url: url.baseUrl)
+    func buildRequest(from route: EndPoint) throws -> URLRequest {
+        let request = URLRequest(url: route.baseUrl)
         return request
     }
 }
