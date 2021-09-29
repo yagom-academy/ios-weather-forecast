1980fb98e87b138827d3c9ca15a42f2c7eea1346
YongHoonJJo <starctak@gmail.com>
2021-09-29 21:00:50 +0900

    feat: APIError 열거형 타입 구현
    - LocalizedError 프로토콜 채택 및 errorDescription 연산프로퍼티 적용.
    - callGetAPI(_:_:url:completion:) 으로 레이블 수정
    - callGetAPI(_:_:url:completion:) 메서드 내 .failure(Error) 적용

--- WeatherForecast/WeatherForecast/Protocols/APIGetCallable.swift
+++ WeatherForecast/WeatherForecast/Protocols/APIGetCallable.swift
@@ -7,28 +7,60 @@
 
 import Foundation
 
-protocol APIGetCallable: JSONDecodable {
+enum APIError: LocalizedError {
+    case invalidUrl
+    case dataTask
+    case invalidResponse
+    case invalideData
+    case unknown
+    case outOfRange(statusCode: Int)
+    case serverMessage(message: String)
     
+    var errorDescription: String? {
+        switch self {
+        case .invalidUrl:
+            return "Invalid URL"
+        case .invalideData:
+            return "Invalid Data"
+        case .dataTask:
+            return "DataTask Error"
+        case .invalidResponse:
+            return "Invalid Response"
+        case .unknown:
+            return "Api Unknown Error"
+        case .outOfRange(let statusCode):
+            return "status: \(statusCode)"
+        case .serverMessage(let message):
+            return message
+        }
+    }
+}
+
+protocol APIGetCallable: JSONDecodable {
+   
 }
 
 extension APIGetCallable {
     func callGetAPI<T, S>(_ responseType: T.Type,
                           _ errorType: S.Type,
                           url: String,
-                          _ completion: @escaping (Result<T, Error>) -> Void
+                          completion: @escaping (Result<T, Error>) -> Void
     ) where T: Decodable, S: Decodable & Error {
         let successRange = 200...299
         guard let url = URL(string: url) else {
+            completion(.failure(APIError.invalidUrl))
             return
         }
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             do {
                 guard error == nil else {
+                    completion(.failure(APIError.dataTask))
                     return
                 }
                 
                 guard let httpResponse = response as? HTTPURLResponse else {
+                    completion(.failure(APIError.invalidResponse))
                     return
                 }
                 
@@ -38,17 +70,22 @@ extension APIGetCallable {
                         completion(.failure(parsedError))
                         return
                     }
+                    
+                    completion(.failure(APIError.outOfRange(statusCode: httpResponse.statusCode)))
                     return
                 }
                 
                 guard let data = data else {
+                    completion(.failure(APIError.invalideData))
                     return
                 }
                 
                 let parsedData = try self.decodedJSON(T.self, from: data)
                 completion(.success(parsedData))
+            } catch let parsingError as ParsingError {
+                completion(.failure(parsingError))
             } catch {
-                completion(.failure(error))
+                completion(.failure(APIError.unknown))
             }
         }.resume()
     }
