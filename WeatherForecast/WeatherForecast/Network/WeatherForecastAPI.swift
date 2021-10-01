83b65b10487dc22cbc02ecc9052c00838ead231f
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-10-01 15:38:00 +0900

    feat: WeatherForecastAPI 구현

--- WeatherForecast/WeatherForecast/Network/WeatherForecastAPI.swift
+++ WeatherForecast/WeatherForecast/Network/WeatherForecastAPI.swift
@@ -14,26 +14,63 @@ import Foundation
 
 // ???
 // api.openweathermap.org/data/2.5/abcd?asd={asd}&bcd={bcd}....
+
 enum WeatherForecastAPI {
     static let baseURL = "api.openweathermap.org/data/2.5/"
     static let APIKey = "a529112a5512a01db04290ddf7b83639"
 
-    case getDailyWeather(coord: Coordinate)
-    case getWeeklyWeather(coord: Coordinate)
+    enum EndPoint: CustomStringConvertible {
+        case daily
+        case weekly
 
-    var urlComponents: String {
-        var components: URLComponents? // = URLComponents(string: Self.baseURL)
-        let latitude = URLQueryItem(name: "lat", value: "{lat}")
-        let longitude = URLQueryItem(name: "lon", value: "{lon}")
-        switch self {
-        case .getDailyWeather(let coord):
-            components = URLComponents(string: Self.baseURL + "weather")
-        case .getWeeklyWeather(let coord):
-            components = URLComponents(string: Self.baseURL + "forecast")
+        var description: String {
+            switch self {
+            case .daily:
+                return "weather"
+            case .weekly:
+                return "forecast"
+            }
         }
     }
 
-    func makeURL(url: BaseWeather, by parameters: )
-}
+    enum Parameters {
+        case coordinate(coord: Coordinate)
+    }
 
- WeatherURLComponent
+    case getWeather(endPoint: EndPoint, params: Parameters)
+    var urlComponents: URL {
+        var component: URLComponents?
+        switch self {
+        case .getWeather(let endPoint, let params):
+            component = URLComponents(string: Self.baseURL + String(describing: endPoint))
+
+            switch params {
+            case .coordinate(let coord):
+                let latitude = URLQueryItem(name: "lat", value: "\(coord.latitude)")
+                let longitude = URLQueryItem(name: "lon", value: "\(coord.longitude)")
+                component?.queryItems = [latitude, longitude]
+            }
+            component?.queryItems?.append(URLQueryItem(name: "appid", value: Self.APIKey))
+        }
+        guard let url = component?.url else {
+            fatalError()
+        }
+        return url
+    }
+//    case getDailyWeather(coord: Coordinate)
+//    case getWeeklyWeather(coord: Coordinate)
+//
+//    var urlComponents: String {
+//        var components: URLComponents? // = URLComponents(string: Self.baseURL)
+//        let latitude = URLQueryItem(name: "lat", value: "{lat}")
+//        let longitude = URLQueryItem(name: "lon", value: "{lon}")
+//        switch self {
+//        case .getDailyWeather(let coord):
+//            components = URLComponents(string: Self.baseURL + "weather")
+//        case .getWeeklyWeather(let coord):
+//            components = URLComponents(string: Self.baseURL + "forecast")
+//        }
+//    }
+//
+//    func makeURL(url: BaseWeather, by parameters: )
+}
--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -10,6 +10,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        WeatherForecastAPI.getWeather(endPoint: .daily, params: .coordinate(coord: Coordinate(longitude: 1.0, latitude: 2.0)))
+            .urlComponents
+        WeatherForecastAPI.getWeather(endPoint: .weekly, params: .coordinate(coord: Coordinate(longitude: 1.0, latitude: 2.0)))
+            .urlComponents
     }
+
+
 }
 
