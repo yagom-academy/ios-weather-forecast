c4782c6dec1345fe7a34f06d50076a85f852383e
soll4u <dasollpark@gmail.com>
2021-10-04 23:12:15 +0900

    feat: APIKey를 저장하는 WeatherInfo.plist 추가
    * WeatherAPI에서 APIKey를 불러오는 연산 프로퍼티 apiKey 추가

--- WeatherForecast/WeatherForecast/Utility/WeatherAPI.swift
+++ WeatherForecast/WeatherForecast/Utility/WeatherAPI.swift
@@ -8,9 +8,21 @@
 import Foundation
 
 enum WeatherAPI {
+    
+    private static var apiKey: String {
+        guard let filePath = Bundle.main.path(forResource: "WeatherInfo", ofType: "plist") else {
+            fatalError("Couldn't find file 'WeatherInfo.plist'.")
+        }
+        let plist = NSDictionary(contentsOfFile: filePath)
+        guard let value = plist?.object(forKey: "APIKey") as? String else {
+            fatalError("Couldn't find key.")
+        }
+        return value
+    }
+    
     private static let scheme = "https"
     private static let host = "api.openweathermap.org"
-    private static let appkey = ""
+    private static let appkey = WeatherAPI.apiKey
     
     case current(CurrentData)
     case fiveday(FiveDayData)
