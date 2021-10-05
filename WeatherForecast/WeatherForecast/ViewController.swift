e473246366dbbdb0b1613af1e9c2edb4871dafb2
yaewonLee <yw990324@gmail.com>
2021-10-05 14:16:15 +0900

    chore: 사용되지 않는 코드 삭제

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -11,23 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        WeatherForecastAPI.getWeather(endPoint: .daily, params: .coordinate(coord: Coordinate(longitude: 1.0, latitude: 2.0)))
-            .urlComponents
-        WeatherForecastAPI.getWeather(endPoint: .weekly, params: .coordinate(coord: Coordinate(longitude: 1.0, latitude: 2.0)))
-            .urlComponents
-
-        if let path = Bundle.main.path(forResource: "sampleOfCurrent", ofType: "json") {
-            let jsonData = try? String(contentsOfFile: path).data(using: .utf8)
-            jsonData.flatMap {
-                do {
-                    let result = try decoder.decode($0, to: TodayWeatherInfo.self)
-                    print(result)
-                } catch {
-                    print("?")
-                }
-            }
-        }
     }
-
 }
 
