49fd5d273b276ca3eba15ebcbe1c45f72afd2562
Joey Hong <isjoeyful@gmail.com>
2021-10-05 22:55:07 +0900

    feat: 주소를 반환하는 getAddress(of:completionHandler) 구현
    * 위도와 경도를 받아 지명을 딕셔너리 형태로 반환
    * 사용할 지명을 Address 타입에 city, street1, street2 케이스로 정리

--- WeatherForecast/WeatherForecast/Utils/LocationManager.swift
+++ WeatherForecast/WeatherForecast/Utils/LocationManager.swift
@@ -29,6 +29,25 @@ class LocationManager: NSObject {
         }
     }
     
+    func getAddress(of location: CLLocation?, completionHandler: @escaping (Result<[Address:String?], Error>) -> Void) {
+        guard let validLocation = location else { return }
+        
+        let converter = CLGeocoder()
+        converter.reverseGeocodeLocation(validLocation, preferredLocale: .current) { (placemarks, error) in
+            if let error = error {
+                completionHandler(.failure(error))
+            }
+            
+            let placemark = placemarks?.first
+            let address: [Address:String?] = [
+                .city: placemark?.administrativeArea,
+                .street1: placemark?.locality,
+                .street2: placemark?.thoroughfare
+            ]
+            
+            completionHandler(.success(address))
+        }
+    }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
