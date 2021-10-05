046a460ddb3a0a0ed51f93f7824e8dacdf34bbd0
yaewonLee <yw990324@gmail.com>
2021-10-05 14:17:30 +0900

    refactor: 커스텀에러 삭제 및 에러핸들링 케이스 추가

--- WeatherForecast/WeatherForecast/Network/Parser.swift
+++ WeatherForecast/WeatherForecast/Network/Parser.swift
@@ -2,14 +2,10 @@
 //  Parser.swift
 //  WeatherForecast
 //
-//  Created by kjs on 2021/10/01.
+//  Created by 샬롯, 수박, 루얀 on 2021/10/01.
 //
 
 import Foundation
-
-enum ParsingError: Error {
-    case decodingFail
-}
 
 struct Parser {
     let decoder = JSONDecoder()
@@ -18,8 +14,14 @@ struct Parser {
         do {
             let parsedData = try decoder.decode(model, from: data)
             return parsedData
-        } catch {
-            throw ParsingError.decodingFail
+        } catch DecodingError.dataCorrupted(let context) {
+            throw DecodingError.dataCorrupted(context)
+        } catch DecodingError.keyNotFound(let codingKey, let context) {
+            throw DecodingError.keyNotFound(codingKey, context)
+        } catch DecodingError.typeMismatch(let type, let context) {
+            throw DecodingError.typeMismatch(type, context)
+        } catch DecodingError.valueNotFound(let value, let context) {
+            throw DecodingError.valueNotFound(value, context) 
         }
     }
 }
