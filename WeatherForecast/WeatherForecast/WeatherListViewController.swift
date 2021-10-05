e323d4a8474831fddf5f82fcd366a1394c0d8858
jaemuyeo <jemking94@gmail.com>
2021-10-05 20:28:41 +0900

    feat: 위치 정보를 가져와서 주소로 변경하는 코드 구현
    - 좌표를 가져오는 bringCoordinates() 메서드 구현
    - 좌표를 주소로 변환해주는 converToAddress(with:)메서드 구현
    - viewDidLoad() 메서드에 필요한 메서드 호출

--- WeatherForecast/WeatherForecast/WeatherListViewController.swift
+++ WeatherForecast/WeatherForecast/WeatherListViewController.swift
@@ -8,17 +8,16 @@ import UIKit
 import CoreLocation
 
 class WeatherListViewController: UIViewController {
-
     private var locationManager: CLLocationManager!
     private var currentLocation: CLLocationCoordinate2D!
-    private var latitude: Double?
-    private var longitude: Double?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        generateLocationManager()
+        bringCoordinates()
+        converToAddress(with: generateCurrentCoordinate())
     }
-
 }
 
 extension WeatherListViewController: CLLocationManagerDelegate {
@@ -53,5 +52,36 @@ extension WeatherListViewController {
         guard let latitude = manager.latitude, let longitude = manager.longitude else { return CLLocation() }
         let location = CLLocation(latitude: latitude, longitude: longitude)
         return location
+    }
+    
+    private func bringCoordinates() {
+        if CLLocationManager.locationServicesEnabled() {
+            let currentCoordinate = locationManager.location?.coordinate
+            
+            locationManager.startUpdatingLocation()
+            guard let lat = currentCoordinate?.latitude, let lon = currentCoordinate?.longitude else {
+                return
+            }
+            WeatherDataManager.shared.latitude = lat
+            WeatherDataManager.shared.longitude = lon
+        }
+    }
+    
+    private func converToAddress(with coordinate: CLLocation) {
+        let geoCoder = CLGeocoder()
+        let locale = Locale(identifier: "ko_kr")
+        
+        geoCoder.reverseGeocodeLocation(coordinate, preferredLocale: locale) { placemark, error in
+            if error != nil {
+                print(error)
+                return
+            }
+            if let administrativeArea = placemark?.first?.administrativeArea {
+                print(administrativeArea)
+            }
+            if let locality = placemark?.first?.locality {
+                print(locality)
+            }
+        }
     }
 }
