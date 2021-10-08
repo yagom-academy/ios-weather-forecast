cea0fece5566a9e69acdc6c5053d3c1d0f8cd393
Hosinging <pdhou23@gmail.com>
2021-10-08 19:37:08 +0900

    chore: 잘못 삭제한 메서드 복구

--- WeatherForecast/WeatherForecast/Controllers/WeatherListViewController.swift
+++ WeatherForecast/WeatherForecast/Controllers/WeatherListViewController.swift
@@ -15,6 +15,7 @@ class WeatherListViewController: UIViewController {
         super.viewDidLoad()
         
         generateLocationManager()
+        bringCoordinates()
         convertToAddress()
     }
 }
@@ -25,7 +26,6 @@ extension WeatherListViewController: CLLocationManagerDelegate {
         case .authorizedWhenInUse, .authorizedAlways:
             currentLocation = manager.location?.coordinate
             guard let currentLocation = currentLocation else { return }
-            currentLocation
             WeatherDataManager.shared.longitude = currentLocation.longitude
             WeatherDataManager.shared.latitude = currentLocation.latitude
             WeatherDataManager.shared.fetchCurrentWeather()
@@ -53,6 +53,20 @@ extension WeatherListViewController {
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
+    
+    private func bringCoordinates() {
+           if CLLocationManager.locationServicesEnabled() {
+               guard let locationManager = locationManager else { return }
+               let currentCoordinate = locationManager.location?.coordinate
+
+               locationManager.startUpdatingLocation()
+               guard let lat = currentCoordinate?.latitude, let lon = currentCoordinate?.longitude else {
+                   return
+               }
+               WeatherDataManager.shared.latitude = lat
+               WeatherDataManager.shared.longitude = lon
+           }
+       }
     
     private func convertToAddress() {
         let coordinate = CLLocation(latitude: unWrappedCoordinate().latitude, longitude: unWrappedCoordinate().longitude)
