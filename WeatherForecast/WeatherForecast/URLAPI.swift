7be92f60c72a0343b68e13bb5c515e64b19615c3
ohsg0226 <ohsg0226@naver.com>
2021-10-08 19:37:17 +0900

    feat: URLRequest를 반환하는 configure method 추가

--- WeatherForecast/WeatherForecast/URLAPI.swift
+++ WeatherForecast/WeatherForecast/URLAPI.swift
@@ -51,36 +51,17 @@ enum WeatherRequest: TargetType {
     }
     
     func configure() -> URLRequest? {
-        <#code#>
+        let apiKey = "1af72e89e05d364984fe32463122135f"
+        var urlComponents = URLComponents(string: self.baseURL)
+        urlComponents?.path = self.path
+        let lat = URLQueryItem(name: "lat", value: self.query.0.description)
+        let lon = URLQueryItem(name: "lon", value: self.query.1.description)
+        let appId = URLQueryItem(name: "appid", value: apiKey)
+        urlComponents?.queryItems = [lat, lon, appId]
+        guard let url = urlComponents?.url else {
+            return nil
+        }
+        let urlRequest = URLRequest(url: url)
+        return urlRequest
     }
 }
-
-//enum URLAPI {
-//    case getCurrent
-//    case getForecast
-//
-//    var path: String {
-//        switch self {
-//        case .getCurrent:
-//            return "/data/2.5/weather"
-//        case .getForecast:
-//            return "/data/2.5/forecast"
-//        }
-//    }
-//}
-//
-//extension URLAPI {
-//    func configure(urlAPI: URLAPI, latitude: Double, longitude: Double) -> URL? {
-//        let apiKey = "1af72e89e05d364984fe32463122135f"
-//        var urlComponents = URLComponents()
-//        urlComponents.scheme = "https"
-//        urlComponents.host = "api.openweathermap.org"
-//        urlComponents.path = urlAPI.path
-//        let lat = URLQueryItem(name: "lat", value: latitude.description)
-//        let lon = URLQueryItem(name: "lon", value: longitude.description)
-//        let appId = URLQueryItem(name: "appid", value: apiKey)
-//        urlComponents.queryItems = [lat, lon, appId]
-//
-//        return urlComponents.url
-//    }
-//}
