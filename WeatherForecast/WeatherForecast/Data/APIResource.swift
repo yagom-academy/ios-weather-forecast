6da263965d03740aa74fe6d753af16f7edb4309b
jaemuyeo <jemking94@gmail.com>
2021-10-08 17:22:21 +0900

    feat: 쿼리 파라미터 타입별 분리
    - APIResource.swift 파일 생성
    - ParamName, AppID, Units, Lang 타입 생성 및 상수 저장

---
+++ WeatherForecast/WeatherForecast/Data/APIResource.swift
@@ -0,0 +1,24 @@
+//
+//  APIResource.swift
+//  WeatherForecast
+//
+//  Created by 잼킹 on 2021/10/08.
+//
+
+import Foundation
+
+enum ParamName: String {
+    case lat, lon, appid, units, lang
+}
+
+enum AppID {
+    static let jamkingID = "af361cc4ac7bf412119174d64ba296ff"
+}
+
+enum Units {
+    static let metric = "metric"
+}
+
+enum Lang {
+    static let korea = "kr"
+}
