220a84b28fe0cf6344b7c4ede93aa7199bcf8697
Hosinging <pdhou23@gmail.com>
2021-10-08 17:39:22 +0900

    feat: URL생성에 필요한 URLResource타입 구현

---
+++ WeatherForecast/WeatherForecast/Data/URLResource.swift
@@ -0,0 +1,24 @@
+//
+//  URLResource.swift
+//  WeatherForecast
+//
+//  Created by 잼킹, 호싱잉 on 2021/10/08.
+//
+
+import Foundation
+
+struct URLResource {
+    let scheme = "https"
+    let host = "api.openweathermap.org"
+    
+    enum PathType: String {
+        case current = "/data/2.5/weather"
+        case fiveDay = "/data/2.5/forecast"
+    }
+
+    struct QueryParam {
+        let name: String
+        let value: String
+    }
+}
+
