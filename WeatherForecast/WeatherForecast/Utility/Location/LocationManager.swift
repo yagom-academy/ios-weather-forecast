56cd215165e56177ae30c1572111fea2096a2a98
soll4u <dasollpark@gmail.com>
2021-10-12 03:40:35 +0900

    feat: 위치 정보를 받아오는 LocationManager 타입 추가

--- WeatherForecast/WeatherForecast/Utility/Location/LocationManager.swift
+++ WeatherForecast/WeatherForecast/Utility/Location/LocationManager.swift
@@ -5,3 +5,36 @@
 //  Created by Soll on 2021/10/08.
 //
 
+import Foundation
+import CoreLocation
+
+final class LocationManager: NSObject, CLLocationManagerDelegate {
+    
+    let manager = CLLocationManager()
+    var completion: ((CLLocation) -> Void)?
+    
+    override init() {
+        super.init()
+        
+        manager.requestWhenInUseAuthorization()
+        manager.delegate = self
+    }
+    
+    public func getUserLocation(completion: @escaping (CLLocation) -> Void) {
+        self.completion = completion
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        completion?(location)
+        manager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
