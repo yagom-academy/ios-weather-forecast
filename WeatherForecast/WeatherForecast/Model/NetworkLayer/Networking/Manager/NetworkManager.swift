44b14d2669fe04adaf7ba10a3f46934edf03ff21
Ldoy <leedoy7989@gmail.com>
2021-10-11 19:08:29 +0900

    refactor: 필요없어진 APIError 열거형 삭제

--- WeatherForecast/WeatherForecast/Model/NetworkLayer/Networking/Manager/NetworkManager.swift
+++ WeatherForecast/WeatherForecast/Model/NetworkLayer/Networking/Manager/NetworkManager.swift
@@ -8,20 +8,6 @@
 import Foundation
 
 final class NetworkManager {
-    enum APIError: Error, LocalizedError {
-        case filePathError
-        case plistError
-        
-        var description: String {
-            switch self {
-            case .filePathError:
-                return "FilePath doesn't exist"
-            case .plistError:
-                return "Plist's name doesn't exist"
-            }
-        }
-    }
-    
     private let router = Router<WeatherApi>()
     private let apiKey = "9cda367698143794391817f65f81c76e"
     var openWeahterApiKey: String {
