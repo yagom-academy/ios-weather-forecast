b3c4631a3945f36446af6230e02a0cea3207579d
YesCoach <sbsii1020@gmail.com>
2021-10-13 02:12:03 +0900

    refactor: Fetching Model 중 사용하지 않는 값 삭제

--- WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
+++ WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
@@ -10,24 +10,15 @@ import Foundation
 struct CurrentWeather: Decodable {
     let coordinate: Coordinate
     let weather: [Weather]
-    let base: String
     let main: Main
-    let wind: Wind
-    let clouds: Clouds
-    let rain: Rain?
-    let snow: Snow?
     let dataReceivingTime: TimeInterval
-    let system: System
-    let timezone: TimeInterval
     let id: Int
     let name: String
-    let cod: Int
 
     enum CodingKeys: String, CodingKey {
-        case weather, base, main, wind, clouds, rain, snow, timezone, id, name, cod
+        case weather, main, id, name
         case coordinate = "coord"
         case dataReceivingTime = "dt"
-        case system = "sys"
     }
 
     struct Coordinate: Decodable {
@@ -56,44 +47,5 @@ struct CurrentWeather: Decodable {
             case tempMin = "temp_min"
             case tempMax = "temp_max"
         }
-    }
-
-    struct Wind: Decodable {
-        let speed: Double
-        let deg: Double
-        let gust: Double
-    }
-
-    struct Clouds: Decodable {
-        let all: Int
-    }
-
-    struct Rain: Decodable {
-        let oneHour: Double?
-        let threeHour: Double?
-
-        enum CodingKeys: String, CodingKey {
-            case oneHour = "1h"
-            case threeHour = "3h"
-        }
-    }
-
-    struct Snow: Decodable {
-        let oneHour: Double?
-        let threeHour: Double?
-
-        enum CodingKeys: String, CodingKey {
-            case oneHour = "1h"
-            case threeHour = "3h"
-        }
-    }
-
-    struct System: Decodable {
-        let type: Int
-        let id: Int
-        let message: Double?
-        let country: String
-        let sunrise: TimeInterval
-        let sunset: TimeInterval
     }
 }
