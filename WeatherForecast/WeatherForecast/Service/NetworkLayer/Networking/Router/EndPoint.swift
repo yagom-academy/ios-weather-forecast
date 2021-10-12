b7ffe9d2191cf25159d2a3cda5ef20de2b1ff5e6
Ldoy <leedoy7989@gmail.com>
2021-10-12 19:35:37 +0900

    feat: EndPoint에 url경로 추가하기 위한 Path 타입 추가

--- WeatherForecast/WeatherForecast/Service/NetworkLayer/Networking/Router/EndPoint.swift
+++ WeatherForecast/WeatherForecast/Service/NetworkLayer/Networking/Router/EndPoint.swift
@@ -11,10 +11,17 @@ protocol EndPoint {
     var httpTask: HTTPTask { get }
     var httpMethod: HTTPMethod { get }
     var baseUrl: URL { get }
+    var path: Path { get }
 }
 
-struct WeatherApi: EndPoint {
+struct OpenWeatherApi: EndPoint {
     var httpTask: HTTPTask
     var httpMethod: HTTPMethod
     var baseUrl: URL
+    var path: Path
+}
+
+enum Path: String {
+    case weather
+    case forecast
 }
