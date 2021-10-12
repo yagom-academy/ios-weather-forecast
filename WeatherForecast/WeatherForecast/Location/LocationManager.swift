f358b2c80b89360a6194ef73d4a2289ac3cbd8ca
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-10-12 22:55:02 +0900

    feat: 현재 위치의 좌표와 주소 요청하는 메소드 구현, 이를 활용하여 날씨정보 요청하는 샘플 코드 작성

---
+++ WeatherForecast/WeatherForecast/Location/LocationManager.swift
@@ -0,0 +1,51 @@
+//
+//  LocationManager.swift
+//  WeatherForecast
+//
+//  Created by kjs on 2021/10/12.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject {
+    static let locationUpdated = NSNotification.Name(rawValue: "locationUpdated")
+    private let coreLocation = CLLocationManager()
+    private let geocoder = CLGeocoder()
+
+    override init() {
+        super.init()
+
+        coreLocation.delegate = self
+        coreLocation.requestWhenInUseAuthorization()
+    }
+
+    func requestLocation() {
+        coreLocation.requestLocation()
+    }
+
+    func convertToAddress(
+        from location: CLLocation,
+        completionHandler: @escaping ([CLPlacemark]?, Error?) -> Void
+    ) {
+        geocoder.reverseGeocodeLocation(
+            location,
+            preferredLocale: .current,
+            completionHandler: completionHandler
+        )
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // TODO: - 권한에 획득 실패에 따른 분기 처리 언젠가는 해야함
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        NotificationCenter.default.post(name: Self.locationUpdated, object: locations)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // TODO: - 좌표 획득 실패에 따른 분기 처리 언젠가는 해야함
+    }
+}
--- WeatherForecast/WeatherForecast/ViewController/Main/MainViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController/Main/MainViewController.swift
@@ -5,34 +5,59 @@
 // 
 
 import UIKit
+import CoreLocation
 
 class MainViewController: UIViewController {
+    let locationManager = LocationManager()
+    let networkManager = NetworkManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // MARK: - 현재 날씨 요청 코드입니다.
-        NetworkManager().request(
-            endpoint: .daily,
-            parameters: ["lat": 35, "lon": 139]
-        ) { (result: Result<TodayWeatherInfo, Error>) in
-            switch result {
-            case .success(let model):
-                print(model)
-            case .failure(let error):
-                print(error)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        NotificationCenter.default.addObserver(
+            forName: LocationManager.locationUpdated,
+            object: nil,
+            queue: .main
+        ) { notification in
+            guard let locations = notification.object as? [CLLocation],
+                  let location = locations.last else {
+                return
+            }
+            let coordinate = location.coordinate
+
+            self.locationManager.convertToAddress(from: location) { placeMarks, error in
+                print(placeMarks)
+            }
+
+            self.networkManager.request(
+                endpoint: .daily,
+                parameters: ["lat": coordinate.latitude, "lon": coordinate.longitude]
+            ) { (result: Result<TodayWeatherInfo, Error>) in
+                switch result {
+                case .success:
+                    print("daily success")
+                case .failure:
+                    print("daily failure")
+                }
+            }
+
+            self.networkManager.request(
+                endpoint: .weekly,
+                parameters: ["lat": coordinate.latitude, "lon": coordinate.longitude]
+            ) { (result: Result<WeeklyWeatherForecast, Error>) in
+                switch result {
+                case .success:
+                    print("weekly success")
+                case .failure:
+                    print("weekly failure")
+                }
             }
         }
-        // MARK: - 주간 일기 예보 요청 코드입니다.
-        NetworkManager().request(
-            endpoint: .weekly,
-            parameters: ["lat": 35, "lon": 139]
-        ) { (result: Result<WeeklyWeatherForecast, Error>) in
-            switch result {
-            case .success(let model):
-                print(model)
-            case .failure(let error):
-                print(error)
-            }
-        }
+        
+        locationManager.requestLocation()
     }
 }
