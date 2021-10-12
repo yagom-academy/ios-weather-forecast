2d56109562db9bcf3b15ca55fa28d7eea135d4bc
Ldoy <leedoy7989@gmail.com>
2021-10-12 19:40:57 +0900

    feat: HTTP응답코드 확인하는 메서드 추가
    - route 매게변수의 path속성을 url에 추가하는 부분 구현

--- WeatherForecast/WeatherForecast/Service/NetworkLayer/Networking/Router/NetworkRouter.swift
+++ WeatherForecast/WeatherForecast/Service/NetworkLayer/Networking/Router/NetworkRouter.swift
@@ -20,33 +20,20 @@ final class Router<EndPointType: EndPoint>: NetworkRouter {
     func request(_ route: EndPointType, _ session: URLSession, _ completionHandler: @escaping (Data) -> ()) {
         let request = self.buildRequest(from: route)
         
-        task = session.dataTask(with: request, completionHandler: { data, response, error in
-            guard error == nil else {
+        task = session.dataTask(with: request, completionHandler: { [weak self] data, response, error in
+            guard let `self` = self else {
                 return
             }
             
-            guard let response = response as? HTTPURLResponse else {
+            guard let response = response as? HTTPURLResponse, self.checkStatusCode(response: response) else {
                 return
             }
             
-            switch response.statusCode {
-            case 200..<300: break
-            case 400:
-                print("잘못된 요청입니다.")
-                return
-            case 401:
-                print("인증이 잘못되었습니다.")
-                return
-            case 403:
-                print("해당 정보에 접근할 수 없습니다. ")
-                return
-            case 500...:
-                print("서버에러")
-                return
-            default:
+            guard let data = data else {
                 return
             }
             
+            completionHandler(data)
         })
         
         self.task?.resume()
@@ -56,8 +43,31 @@ final class Router<EndPointType: EndPoint>: NetworkRouter {
         self.task?.cancel()
     }
     
+    private func checkStatusCode(response: HTTPURLResponse) -> Bool {
+        switch response.statusCode {
+        case 200:
+            return true
+        case 400:
+            print("잘못된 요청입니다.")
+            return false
+        case 401:
+            print("인증이 잘못되었습니다.")
+            return false
+        case 403:
+            print("해당 정보에 접근할 수 없습니다. ")
+            return false
+        case 500...:
+            print("서버에러")
+            return false
+        default:
+            print("네트워크 요청을 확인 해 보세요")
+            return false
+        }
+    }
+    
     private func buildRequest(from route: EndPointType) -> URLRequest {
         var request = URLRequest(url: route.baseUrl)
+        request.url?.appendPathComponent(route.path.rawValue, isDirectory: false)
         
         switch route.httpTask {
         case .request(withUrlParameters: let urlParameter):
@@ -67,7 +77,7 @@ final class Router<EndPointType: EndPoint>: NetworkRouter {
                 print(error)
             }
         }
-        
+
         return request
     }
 }
