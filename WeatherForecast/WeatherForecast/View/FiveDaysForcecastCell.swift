202014f50544e4b920a5b38fb0aba22d4e82d6fc
Ldoy <leedoy7989@gmail.com>
2021-10-12 19:36:20 +0900

    feat: custom cell 구현 및 cell에 데이터 전달하는 CellHolder구현

--- WeatherForecast/WeatherForecast/View/FiveDaysForcecastCell.swift
+++ WeatherForecast/WeatherForecast/View/FiveDaysForcecastCell.swift
@@ -7,17 +7,53 @@
 
 import UIKit
 
+struct CellHolder {
+    let dateLabelText: String?
+    let temperatureText: String?
+    var iconImage: UIImage?
+}
+
 class FiveDaysForcecastCell: UITableViewCell {
 
-    private lazy var dateLabel = UILabel()
-    private lazy var temperatureLabel = UILabel()
-    private lazy var weatherIconImageView = UIImageView()
+    func configure(_ holder: CellHolder) {
+        self.dateLabel.text = holder.dateLabelText
+        self.temperatureLabel.text = holder.temperatureText
+        self.weatherIconImageView.image = holder.iconImage
+    }
+    
+    private lazy var dateLabel: UILabel = {
+        let dateLabel = UILabel()
+        dateLabel.textColor = .black
+        dateLabel.textAlignment = .center
+        dateLabel.adjustsFontForContentSizeCategory = true
+        
+        return dateLabel
+    }()
+    
+    private lazy var temperatureLabel: UILabel = {
+        let tempLabel = UILabel()
+        tempLabel.textColor = .black
+        tempLabel.textAlignment = .center
+        tempLabel.adjustsFontForContentSizeCategory = true
+        
+        return tempLabel
+    }()
+    
+    private lazy var weatherIconImageView: UIImageView = {
+        let weatherIcon = UIImageView()
+        if let placeHolderImage = UIImage(named: "cat") {
+            weatherIcon.image = placeHolderImage
+            return weatherIcon
+        }
+        return UIImageView()
+    }()
+    
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [self.dateLabel, self.temperatureLabel, self.weatherIconImageView])
         
         stackView.axis = .horizontal
         self.contentView.addSubview(stackView)
-
+        
         stackView.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
         stackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
         stackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
