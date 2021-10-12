be4ee2d050f36bc249c94cb1fd4d2bc800407c92
Ldoy <leedoy7989@gmail.com>
2021-10-12 19:39:50 +0900

    feat: MVVM패턴 에서 Observer 역할을 하는 Box<T> 구현

---
+++ WeatherForecast/WeatherForecast/Observer/Boxing.swift
@@ -0,0 +1,28 @@
+//
+//  Boxing.swift
+//  WeatherForecast
+//
+//  Created by Do Yi Lee on 2021/10/12.
+//
+
+import Foundation
+
+final class Box<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
