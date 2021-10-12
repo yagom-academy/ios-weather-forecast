2c556000b00ac68b1e4f6cbcfa19e9a99ca99e65
YongHoonJJo <starctak@gmail.com>
2021-10-12 20:52:03 +0900

    poc: LocationManager 관련 기능(델리게이트)등 확인.

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -5,11 +5,36 @@
 // 
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLocationInfo()
+    }
+    
+    func setupLocationInfo() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let currentLocation = locations.last else {
+            return
+        }
+        
+        let longitude = currentLocation.coordinate.longitude
+        let latitude = currentLocation.coordinate.latitude
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         
     }
 }
