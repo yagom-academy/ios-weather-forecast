ea51c47ec111cef58210a2c4738ae2253740e4a7
산체스 <xognsl87@gmail.com>
2021-10-13 16:46:40 +0900

    feat: viewController 에서 날씨정보를 가져오는 코드 추가
    - 주소를 가져오는 getUserAddress() 추가
    - 날씨정보를 네트워크로 가져오는 getCurrentWeatherImpormation() 추가
    - 날씨정보를 네트워크로 가져오는 getFiveDaysWeatherImpormation() 추가
    - 날씨정보 및 주소를 한번에 최신화 할 수 있는 processWeatherImpormation() 추가

--- WeatherForecast/WeatherForecast/Controller/WeatherImpomationViewController.swift
+++ WeatherForecast/WeatherForecast/Controller/WeatherImpomationViewController.swift
@@ -5,30 +5,75 @@
 // 
 
 import UIKit
-import CoreLocation
 
 class WeatherImpormationViewController: UIViewController {
     private let locationManager = LocationManager()
     private let apiManager = APIManager()
+    private let decodingManager = DecodingManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        processWeatherNetworking()
-        
+        processWeatherImpormation()
     }
-    private func processWeatherNetworking() {
+    
+    private func processWeatherImpormation() {
         locationManager.getUserLocation { [weak self] location in
             guard let self = self else { return }
-            let url = WeatherURL.forecastCoordinates(latitude: location.coordinate.latitude,
-                                                     longitude: location.coordinate.longitude)
-            self.getWeatherImpormation(request: url)
+            let currentWeatherURL =
+            WeatherURL.weatherCoordinates(latitude: location.coordinate.latitude,
+                                          longitude: location.coordinate.longitude)
+            let fiveDaysWeatherURL =
+            WeatherURL.forecastCoordinates(latitude: location.coordinate.latitude,
+                                          longitude: location.coordinate.longitude)
+            self.getCurrentWeatherImpormation(request: currentWeatherURL)
+            self.getFiveDaysWeatherImpormation(request: fiveDaysWeatherURL)
+            self.getUserAddress()
         }
     }
     
-    private func getWeatherImpormation(request: UrlGeneratable) {
-        self.apiManager.requestAPI(resource: APIResource(apiURL: request)) { result in
-            dump(result)
+    private func getUserAddress() {
+        locationManager.getUserAddress { address in
+            switch address {
+            case .success(let data):
+                debugPrint(data)
+            case .failure(let error):
+                self.handlerError(error)
+            }
         }
+    }
+    
+    private func getCurrentWeatherImpormation(request: UrlGeneratable) {
+        self.apiManager.requestAPI(
+            resource: APIResource(apiURL: request)) { result in
+                switch result {
+                case .success(let data):
+                    guard let product =
+                            try? self.decodingManager.decodeJSON(
+                                CurrentWeather.self, from: data) else {
+                                    return
+                                }
+                    debugPrint(product)
+                case .failure(let error):
+                    self.handlerError(error)
+                }
+            }
+    }
+    
+    private func getFiveDaysWeatherImpormation(request: UrlGeneratable) {
+        self.apiManager.requestAPI(
+            resource: APIResource(apiURL: request)) { result in
+                switch result {
+                case .success(let data):
+                    guard let product =
+                            try? self.decodingManager.decodeJSON(
+                                FiveDaysWeather.self, from: data) else {
+                                    return
+                                }
+                    debugPrint(product)
+                case .failure(let error):
+                    self.handlerError(error)
+                }
+            }
     }
 }
