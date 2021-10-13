4e688a06c0ceaf28c0a4cd486d9cce0049a0894f
YongHoonJJo <starctak@gmail.com>
2021-10-14 00:01:19 +0900

    feat: GeocoderManager 타입 추가
    - requestAddress(coordinate:completion:) - 좌표를 통한 주소를 불러오는 메서드 기능 구현.
    - CLLocation 타입 custom init(coordinate:) 구현.

---
+++ WeatherForecast/WeatherForecast/Managers/GeocoderManager.swift
@@ -0,0 +1,39 @@
+//
+//  GeocoderManager.swift
+//  WeatherForecast
+//
+//  Created by YongHoon JJo on 2021/10/13.
+//
+
+import Foundation
+import CoreLocation
+
+class GeocoderManager {
+    private let geocoder = CLGeocoder()
+    
+    func requestAddress(on coordinate: CLLocationCoordinate2D,
+                        completion: @escaping (Result<CLPlacemark, Error>) -> Void) {
+        let targetLocation = CLLocation(coordinate: coordinate)
+        let locale = Locale(identifier: "Ko-kr")
+
+        geocoder.reverseGeocodeLocation(targetLocation, preferredLocale: locale) { (placemarks, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let addresses: [CLPlacemark] = placemarks,
+                  let address = addresses.last else {
+                return
+            }
+                                            
+            completion(.success(address))
+        }
+    }
+}
+
+extension CLLocation {
+    convenience init(coordinate: CLLocationCoordinate2D) {
+        self.init(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    }
+}
