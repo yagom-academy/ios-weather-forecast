f913a7d843b09e5ffcbad58daf77866e512d82b2
soll4u <dasollpark@gmail.com>
2021-10-14 03:20:27 +0900

    feat: FloatingPoint를 String으로 변환하는 메서드 추가
    * NumberFormatter를 사용해서 maxDisits은 1이고
      roundingMode는 halfEven으로 String을 반환함

---
+++ WeatherForecast/WeatherForecast/Utility/Formatter/Formatter+FloatingPoint.swift
@@ -0,0 +1,22 @@
+//
+//  Formatter.swift
+//  WeatherForecast
+//
+//  Created by Soll on 2021/10/14.
+//
+
+import Foundation
+
+extension FloatingPoint {
+    
+    func franctionDisits(
+        maxDisits: Int = 1,
+        roundingMode: NumberFormatter.RoundingMode = .halfEven
+    ) -> String {
+        let formatter = NumberFormatter()
+        
+        formatter.maximumFractionDigits = maxDisits
+        formatter.roundingMode = roundingMode
+        return formatter.string(for: self) ?? "\(self)"
+    }
+}
