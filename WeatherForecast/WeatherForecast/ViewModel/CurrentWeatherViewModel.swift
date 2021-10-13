01e00163f9a8104f8a341303f58710b1e9911ce9
soll4u <dasollpark@gmail.com>
2021-10-14 02:10:12 +0900

    feat: CurrentWeatherViewModel에 프로퍼티 추가
    * 화면에 표시될 String 타입의 프로퍼티 생성

--- WeatherForecast/WeatherForecast/ViewModel/CurrentWeatherViewModel.swift
+++ WeatherForecast/WeatherForecast/ViewModel/CurrentWeatherViewModel.swift
@@ -9,4 +9,10 @@ import UIKit
 
 class CurrentWeatherViewModel {
     
+    var administrativeArea: String = "서울특별시"
+    var locality: String = "세종로"
+    var temperature: String = "-"
+    var minTemperature: String = "-"
+    var maxTemperature: String = "-"
+    var weatherImage: UIImage = UIImage()
 }
