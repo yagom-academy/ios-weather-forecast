6b62cc30f2a59c1747e46531629c36faf2733629
soll4u <dasollpark@gmail.com>
2021-10-14 23:16:33 +0900

    test: CurrentWeatherViewModel의 테스트 출력문 수정

--- WeatherForecast/WeatherForecast/ViewModel/CurrentWeatherViewModel.swift
+++ WeatherForecast/WeatherForecast/ViewModel/CurrentWeatherViewModel.swift
@@ -26,7 +26,7 @@ class CurrentWeatherViewModel {
                let locality = placemark.locality {
                 self.administrativeArea = administrativeArea
                 self.locality = locality
-                print(administrativeArea, locality)
+                print("현재 주소: \(administrativeArea), \(locality)")
             }
         }
         
@@ -35,15 +35,15 @@ class CurrentWeatherViewModel {
             
             if let temperature = currentWeather.mainInformation?.temperature {
                 self.temperature = temperature.franctionDisits()
-                print(temperature.franctionDisits())
+                print("현재 온도: \(temperature.franctionDisits())")
             }
             if let minTemperature = currentWeather.mainInformation?.minimumTemperature {
                 self.minTemperature = minTemperature.franctionDisits()
-                print(minTemperature.franctionDisits())
+                print("최저 온도: \(minTemperature.franctionDisits())")
             }
             if let maxTemperature = currentWeather.mainInformation?.maximumTemperature {
                 self.maxTemperature = maxTemperature.franctionDisits()
-                print(maxTemperature.franctionDisits())
+                print("최고 온도: \(maxTemperature.franctionDisits())")
             }
         }
     }
