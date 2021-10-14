4db71b5c7ede106d1c855bc8fe57b0bb5d8fa021
yoonju lee <rsyjlee@gmail.com>
2021-10-15 02:04:20 +0900

    feat: 테이블뷰 추가 및 테이블뷰에 헤더뷰 등록

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -13,15 +13,39 @@ class ViewController: UIViewController {
     private var currentWeather: CurrentWeather?
     private var fiveDayForecast: FiveDayForecast?
     private var locationManager = CLLocationManager()
+    private var weatherTableView = UITableView()
+    private let cellIdentifier = "ForecastCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startMonitoringVisits()
+        setUpTableView()
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = weatherTableView.dequeueReusableHeaderFooterView(withIdentifier: CurrentWeatherHeader.headerIdentifier) as! CurrentWeatherHeader
+        guard let currentWeather = currentWeather else {
+            return UIView()
+        }
+        
+        return view
+    }
+}
 
 extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
@@ -43,6 +67,23 @@ extension ViewController: CLLocationManagerDelegate {
 }
 
 extension ViewController {
+    private func setUpTableView() {
+        view.addSubview(weatherTableView)
+        view.backgroundColor = .white
+        weatherTableView.dataSource = self
+        weatherTableView.delegate = self
+        weatherTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        weatherTableView.register(CurrentWeatherHeader.self,
+                                  forHeaderFooterViewReuseIdentifier: CurrentWeatherHeader.headerIdentifier)
+        weatherTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            weatherTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            weatherTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            weatherTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            weatherTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
     private func fetchCurrentWeather(latitude: Double, longitude: Double) {
         networkManager.request(WeatherRequest.getCurrentWeather(latitude: latitude, longitude: longitude)) { result in
             switch result {
