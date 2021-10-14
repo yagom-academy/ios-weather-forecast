a15e7b85c7dfce2b51d8adf0764deaa01cb0b504
Ldoy <leedoy7989@gmail.com>
2021-10-14 10:54:09 +0900

    feat: tableViewDataSource에서 cell로 정보 전달하는 로직 구현

--- WeatherForecast/WeatherForecast/Controller/WeatherTableViewDataSource.swift
+++ WeatherForecast/WeatherForecast/Controller/WeatherTableViewDataSource.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class WeatherTableviewDataSource: NSObject, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return Holder.shared.forcast?.list.count ?? .zero
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as? FiveDaysForcecastCell else {
             return UITableViewCell()
         }
+        
+        guard let list = Holder.shared.forcast?.list[indexPath.row] else {
+            return UITableViewCell()
+        }
+
+        var image = UIImage()
+        let urlString = "https://openweathermap.org/img/w/\(list.weather.first?.icon ?? "").png"
+        
+        ImageLoader.downloadImage(reqeustURL: urlString, imageCachingKey: list.date) { requestedIcon in
+            image = requestedIcon
+            DispatchQueue.main.async {
+                cell.configure(image)
+            }
+        }
+        
+        let cellHolder = CellHolder(forcastInformation: list)
+        
+        cell.configure(cellHolder)
         
         return cell
     }
