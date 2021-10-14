2e9a9bdb3dd444cb82423da25f33f7158aab8999
soll4u <dasollpark@gmail.com>
2021-10-14 23:19:44 +0900

    feat: viewDidLoad()호출 시 FiveDayWeatherData데이터 출력

--- WeatherForecast/WeatherForecast/View/ViewController.swift
+++ WeatherForecast/WeatherForecast/View/ViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentWeatherViewModel = CurrentWeatherViewModel()
+    var fiveDayWeatherViewModel = FiveDayWeatherViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         currentWeatherViewModel.reload()
+        fiveDayWeatherViewModel.reload()
     }
 }
