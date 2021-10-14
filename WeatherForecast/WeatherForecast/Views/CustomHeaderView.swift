bd97a5cfde526f03209016b62867cab826d4ef36
hosinging <pdhou23@gmail.com>
2021-10-15 02:02:05 +0900

    feat: 테이블뷰에 표시할 커스텀 셀, 커스텀 헤더뷰 구현

--- WeatherForecast/WeatherForecast/Views/CustomHeaderView.swift
+++ WeatherForecast/WeatherForecast/Views/CustomHeaderView.swift
@@ -9,5 +9,87 @@ import UIKit
 
 class CustomHeaderView: UITableViewHeaderFooterView {
     static let identifier = "headerView"
-
+    
+    var weatherImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "background")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    var addressLabel: UILabel = {
+        let label = UILabel()
+        label.text = "테스트"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var mininumAndMaximumTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.text = "테스트2"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var currentTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.text = "테스트3"
+        label.font = .preferredFont(forTextStyle: .largeTitle)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    var coordinateButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("위치 설정", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return button
+    }()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        configureHeaderView()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHeaderView() {
+        verticalStackView.addArrangedSubview(addressLabel)
+        verticalStackView.addArrangedSubview(mininumAndMaximumTemperatureLabel)
+        verticalStackView.addArrangedSubview(currentTemperatureLabel)
+        
+        contentView.addSubview(weatherImageView)
+        contentView.addSubview(verticalStackView)
+        contentView.addSubview(coordinateButton)
+        
+        NSLayoutConstraint.activate([
+            weatherImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            weatherImageView.widthAnchor.constraint(equalToConstant: 100),
+            weatherImageView.heightAnchor.constraint(equalTo: weatherImageView.widthAnchor),
+            weatherImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            verticalStackView.topAnchor.constraint(greaterThanOrEqualTo: contentView.topAnchor, constant: 5),
+            verticalStackView.leadingAnchor.constraint(equalTo: weatherImageView.trailingAnchor, constant: 10),
+            verticalStackView.bottomAnchor.constraint(greaterThanOrEqualTo: contentView.bottomAnchor, constant: -5),
+            verticalStackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            verticalStackView.trailingAnchor.constraint(greaterThanOrEqualTo: coordinateButton.leadingAnchor, constant: 20),
+            
+            coordinateButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant:-5),
+            coordinateButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10)
+        ])
+    }
 }
--- WeatherForecast/WeatherForecast/Views/CustomTableViewCell.swift
+++ WeatherForecast/WeatherForecast/Views/CustomTableViewCell.swift
@@ -10,15 +10,62 @@ import UIKit
 class CustomTableViewCell: UITableViewCell {
     static let identifier = "cell"
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    var weatherImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleToFill
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    var mininumTemperatureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var dateLabel: UILabel = {
+        let label = UILabel()
+        label.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var horizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        
+        return stackView
+    }()
+    
+    private func configureLayout() {
+        horizontalStackView.addArrangedSubview(dateLabel)
+        horizontalStackView.addArrangedSubview(mininumTemperatureLabel)
+        horizontalStackView.addArrangedSubview(weatherImageView)
+        contentView.addSubview(horizontalStackView)
+        
+            NSLayoutConstraint.activate([
+                
+                horizontalStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+                horizontalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+                horizontalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+                horizontalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20)
+            ])
+        }
+    override func setNeedsLayout() {
+        self.backgroundColor = .clear
+        configureLayout()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+}
 
-        // Configure the view for the selected state
+extension UIImageView {
+    open override var intrinsicContentSize: CGSize {
+        super.intrinsicContentSize
+        return CGSize(width: 40, height: 40)
     }
-
 }
