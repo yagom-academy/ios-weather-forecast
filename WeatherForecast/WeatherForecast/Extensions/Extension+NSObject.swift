fb17ef2a21fb71ea2e827de35667adbdd3af2c9d
YongwooKim <keepingitflow@gmail.com>
2021-10-14 18:50:20 +0900

    fea: 클래스 명을 문자열로 만들어 주는 기능 추가

---
+++ WeatherForecast/WeatherForecast/Extensions/Extension+NSObject.swift
@@ -0,0 +1,17 @@
+//
+//  Extension+NSObject.swift
+//  WeatherForecast
+//
+//  Created by Yongwoo Marco on 2021/10/13.
+//
+
+import Foundation
+
+extension NSObject {
+    class var className: String {
+        return String(describing: self)
+    }
+    var className: String {
+        return type(of: self).className
+    }
+}
