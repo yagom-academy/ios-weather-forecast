e15e197a48dde3aa88efe471800e285f6c224c02
Theo <pdhou23@kakao.com>
2021-10-14 21:40:51 +0900

    feat: 날씨 표시되는 TableView일부 구현 - tableView 클로저로 구현 - tableView CustomHeaderFooterView 일부 구현 - tableView CustomCell 일부 구현

---
+++ WeatherForecast/WeatherForecast/Views/CustomHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomHeaderView.swift
+//  WeatherForecast
+//
+//  Created by Theo on 2021/10/14.
+//
+
+import UIKit
+
+class CustomHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
