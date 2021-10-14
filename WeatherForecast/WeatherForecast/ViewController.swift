b07d3c3d452e87223c3cb9d6130406ffb11f446d
YongHoonJJo <starctak@gmail.com>
2021-10-14 21:06:55 +0900

    feat: locationManager 를 활용한 좌표 구히가 및 좌표를 이용한 날씨 데이터 fetch 로직 적용.
    - 현재 위치 좌표를 가져오기 위한 LocationManager 인스턴스 추가
    - 현재 좌표를 바탕으로 주소정보를 가져오기 위한 GeocoderManager 인스턴스 추가
    - OpenAPI 호출을 위한 APIManager 인스턴스 추가
    - location 수신 이벤트 트리거를 위한 fetchWeatherData() 호출 in viewDidLoad()
    - fetchWeatherAPI<T>(_:weatherURL:completion:) - 현재 날시 및 5일치 날씨를 가져오기 위한 apiManager.requestAPI() 호출 로직 추상화
    - generateResultAction<T>(_:채ㅡㅔㅣㄷ샤ㅐㅜ) - 응답 모델에 따른 result 데이터를 파싱하기 Action 메서드를 구분하여 생성하기 위한 위한 메서드.
    - fetchAddress(coordinate:) - 좌표정보를 통해 주소정보를 가져오는 로직 구현.

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -5,12 +5,84 @@
 // 
 
 import UIKit
-
+import CoreLocation
 
 class ViewController: UIViewController {
+    private let locationManager = LocationManager()
+    private let geocoderManager = GeocoderManager()
+    private let apiManager = APIManager()
+    private var coordinate = CLLocationCoordinate2D() {
+        didSet {
+            fetchWeatherAPI(CurrentWeather.self,
+                            weatherURL: .weatherCoordinates(latitude: coordinate.latitude,
+                                                            longitude: coordinate.longitude),
+                            completion: { data in
+                                dump(data)
+                            })
+            fetchWeatherAPI(FiveDaysWeather.self,
+                            weatherURL: .forecastCoordinates(latitude: coordinate.latitude,
+                                                             longitude: coordinate.longitude),
+                            completion: { data in
+                                dump(data)
+                            })
+            fetchAddress(on: coordinate)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchWeatherData()
     }
 }
 
+extension ViewController {
+    private func fetchWeatherData() {
+        locationManager.requestLocation { coordinate in
+            self.coordinate = coordinate
+        }
+    }
+    
+    private func fetchWeatherAPI<T: Decodable>(
+        _ type: T.Type,
+        weatherURL: WeatherURL,
+        completion: @escaping((T) -> Void)
+    ) {
+        let apiResource = APIResource(apiURL: weatherURL)
+        let weatherResultAction = generateResultAction(T.self, completion: completion)
+        
+        apiManager.requestAPI(resource: apiResource, completion: weatherResultAction)
+    }
+    
+    private func fetchAddress(on coordinate: CLLocationCoordinate2D) {
+        geocoderManager.requestAddress(on: coordinate) { result in
+            switch result {
+            case .success(let address):
+                dump(address)
+            case .failure(let error):
+                self.handlerError(error)
+            }
+        }
+    }
+    
+    private func generateResultAction<T: Decodable>(
+        _ type: T.Type,
+        completion: @escaping (T) -> Void
+    ) -> ((Result<Data, Error>) -> Void) {
+        func resultAction(result: Result<Data, Error>) {
+            do {
+                switch result {
+                case .success(let data):
+                    let decoder = DecodingManager()
+                    let parsedData = try decoder.decodeJSON(T.self, from: data)
+                    completion(parsedData)
+                case .failure(let error):
+                    handlerError(error)
+                }
+            } catch {
+                
+            }
+        }
+        
+        return resultAction
+    }
+}
