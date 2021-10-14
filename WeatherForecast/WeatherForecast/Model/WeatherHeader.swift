c3e90bf177ad07f8a711512daca9654876f0210d
Joey Hong <isjoeyful@gmail.com>
2021-10-14 21:17:24 +0900

    fix: 이전 커밋에서 누락되었던 WeatherHeader.swift 추가

---
+++ WeatherForecast/WeatherForecast/Model/WeatherHeader.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherHeader.swift
+//  WeatherForecast
+//
+//  Created by 홍정아 on 2021/10/14.
+//
+
+import Foundation
+import UIKit.UIImage
+
+struct WeatherHeader: Hashable {
+    let address: String
+    let maxTemperature: String
+    let minTemperature: String
+    let temperature: String
+    let image: UIImage
+    var weathers: [FiveDayWeather.List]
+    
+    init(address: String,
+         minTemperature: String,
+         maxTemperature: String,
+         temperature: String,
+         weatherIcon: UIImage,
+         weathers: [FiveDayWeather.List] = [])
+    {
+        self.address = address
+        self.maxTemperature = maxTemperature
+        self.minTemperature = minTemperature
+        self.temperature = temperature
+        self.image = weatherIcon
+        self.weathers = weathers
+    }
+}
