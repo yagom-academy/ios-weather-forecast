38cdfaf67e31dfda7a9490bdb00d839b8aeb4ee6
KimWanki <seoulkwk95@gmail.com>
2021-10-15 11:58:58 +0900

    feat: add BaseView, WeatherView

---
+++ WeatherForecast/WeatherForecast/Scene/BaseView.swift
@@ -0,0 +1,20 @@
+//
+//  baseView.swift
+//  WeatherForecast
+//
+//  Created by WANKI KIM on 2021/10/15.
+//
+
+import UIKit
+
+class baseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ WeatherForecast/WeatherForecast/Scene/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  WeatherForecast
+//
+//  Created by WANKI KIM on 2021/10/15.
+//
+
+import UIKit
+
+class WeatherView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
