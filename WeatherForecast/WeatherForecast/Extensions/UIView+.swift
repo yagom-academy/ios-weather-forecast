bd828bf86eb8e28a713567ffc86ded5dc77fcccf
KimWanki <seoulkwk95@gmail.com>
2021-10-15 13:32:33 +0900

    feat: implement UIView extension method addSubviews

---
+++ WeatherForecast/WeatherForecast/Extensions/UIView+.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+.swift
+//  WeatherForecast
+//
+//  Created by WANKI KIM on 2021/10/15.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubViews(_ views: UIView...) {
+        for view in views {
+            view.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview(view)
+        }
+    }
+}
