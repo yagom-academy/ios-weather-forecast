0cb61398a095976c3dce1bc007331e41c03e757e
YongwooKim <keepingitflow@gmail.com>
2021-10-15 15:56:24 +0900

    refactor: TableViewCell 제약조건 변경

--- WeatherForecast/WeatherForecast/View/MainTableViewCell.swift
+++ WeatherForecast/WeatherForecast/View/MainTableViewCell.swift
@@ -38,31 +38,34 @@ class MainTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = .clear
-        self.contentView.backgroundColor = .clear
+        self.translatesAutoresizingMaskIntoConstraints = false
         
-        self.contentView.addSubview(containView)
         containView.addArrangedSubview(dateLabel)
         containView.addArrangedSubview(temperatureLabel)
         containView.addArrangedSubview(weatherIconImage)
-                
+        
+        self.contentView.addSubview(containView)
+        
         NSLayoutConstraint.activate([
-            weatherIconImage.widthAnchor.constraint(equalToConstant: 50),
-            weatherIconImage.widthAnchor.constraint(equalToConstant: 50),
+            weatherIconImage.heightAnchor.constraint(equalToConstant: 30),
+            weatherIconImage.widthAnchor.constraint(equalToConstant: 30),
             
-            containView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            containView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 8.0),
             containView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: 8.0),
-            containView.rightAnchor.constraint(equalTo: self.contentView.rightAnchor, constant: 8.0),
-            containView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+            containView.rightAnchor.constraint(equalTo: self.contentView.rightAnchor, constant: 8.0)
+//            containView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: 8.0)
         ])
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configure(_ date: String, temperature: String, image: String) {
+    func configureTexts(_ date: String, temperature: String) {
         dateLabel.text = date
         temperatureLabel.text = temperature
+    }
+    
+    func configureIcon(image: String) {
 //        weatherIconImage.image = image
     }
-
 }
