c0974c2100157727ac89a6ce60efcd3299eea775
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-10-15 21:04:08 +0900

    feat: TableView관련 subView들 추가, 샘플용 이미지 추가
    * MainViewController에서 extension을 활용하여 기능별 그룹핑

--- WeatherForecast/WeatherForecast/ViewController/Main/MainViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController/Main/MainViewController.swift
@@ -10,9 +10,24 @@ import CoreLocation
 final class MainViewController: UIViewController {
     private let locationManager = LocationManager()
     private let networkManager = NetworkManager()
+
+    var tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(
+            MainViewTableViewHeaderView.self,
+            forHeaderFooterViewReuseIdentifier: MainViewTableViewHeaderView.identifier
+        )
+        tableView.register(
+            MainViewTableViewCell.self,
+            forCellReuseIdentifier: MainViewTableViewCell.identifier
+        )
+
+        layoutTableView()
         
         NotificationCenter.default.addObserver(
             forName: LocationManager.locationUpdated,
@@ -26,7 +41,57 @@ final class MainViewController: UIViewController {
         super.viewDidAppear(animated)
         locationManager.requestLocation()
     }
-    
+}
+
+// MARK: - Draw subviews
+extension MainViewController {
+    func layoutTableView() {
+        let safeArea = view.safeAreaLayoutGuide
+
+        view.addSubview(tableView)
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - TableViewDataSource
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: MainViewTableViewCell.identifier
+        ) as? MainViewTableViewCell else {
+            return UITableViewCell()
+        }
+
+        return cell
+    }
+}
+
+// MARK: - TableViewDelegate
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: MainViewTableViewHeaderView.identifier
+        ) as? MainViewTableViewHeaderView else {
+            return UIView()
+        }
+
+        return header
+    }
+}
+
+// MARK: - Networking to get user location and address
+extension MainViewController {
     private func utilizeLocation(_ notification: Notification) {
         guard let location = notification.object as? CLLocation else {
                   return
@@ -36,7 +101,7 @@ final class MainViewController: UIViewController {
         requestDailyWeather(latitude: coordinate.latitude, longitude: coordinate.longitude)
         requestWeeklyWeather(latitude: coordinate.latitude, longitude: coordinate.longitude)
     }
-    
+
     private func requestDailyWeather(latitude: Double, longitude: Double) {
         networkManager.request(
             endpoint: .daily,
@@ -50,21 +115,21 @@ final class MainViewController: UIViewController {
             }
         }
     }
-    
+
     private func requestWeeklyWeather(latitude: Double, longitude: Double) {
         networkManager.request(
             endpoint: .weekly,
             parameters: ["lat": latitude, "lon": longitude]
         ) { (result: Result<WeeklyWeatherForecast, Error>) in
             switch result {
-            case .success:
+            case .success(let weeklyWeatherForecast):
                 print("weekly success")
             case .failure:
                 print("weekly failure")
             }
         }
     }
-    
+
     private func convertToAddress(location: CLLocation) {
         locationManager.convertToAddress(from: location) { placemarks, error in
             print(placemarks)
---
+++ WeatherForecast/WeatherForecast/ViewController/Main/MainViewTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewTableViewCell.swift
+//  WeatherForecast
+//
+//  Created by kjs on 2021/10/15.
+//
+
+import UIKit
+
+class MainViewTableViewCell: UITableViewCell {
+    static let identifier = "MainViewTableViewCell"
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("Error: cell is created on wrong way, Do not use interface builder")
+    }
+}
