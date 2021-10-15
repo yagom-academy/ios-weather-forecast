5f7763001f2be50c986b05713fc31ab74f708f6d
YongwooKim <keepingitflow@gmail.com>
2021-10-16 00:04:18 +0900

    feat: 셀 선택시 색상 변경되는 것 none 처리

--- WeatherForecast/WeatherForecast/View/MainTableViewCell.swift
+++ WeatherForecast/WeatherForecast/View/MainTableViewCell.swift
@@ -40,6 +40,7 @@ final class MainTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = .clear
+        self.selectionStyle = .none
         setUpViewLayout()
         setUpConstraints()
     }
