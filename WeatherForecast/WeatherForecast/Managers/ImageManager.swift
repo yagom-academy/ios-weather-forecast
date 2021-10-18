ec97c1b0dfc587185fc773810378544f80359a00
산체스 <xognsl87@gmail.com>
2021-10-18 21:01:57 +0900

    feat: ImageManager 타입 구현

---
+++ WeatherForecast/WeatherForecast/Managers/ImageManager.swift
@@ -0,0 +1,47 @@
+//
+//  ImageManager.swift
+//  WeatherForecast
+//
+//  Created by tae hoon park on 2021/10/18.
+//
+
+import UIKit
+
+struct ImageManager {
+    private var session: URLSessionProtocol
+    
+    init(session: URLSessionProtocol = URLSession.shared) {
+        self.session = session
+        let configuration = URLSessionConfiguration.default
+        configuration.requestCachePolicy = .returnCacheDataElseLoad
+        URLCache.shared.memoryCapacity = 500 * 1024 * 1024
+        self.session = URLSession(configuration: configuration)
+    }
+    
+    func fetchImage(
+        url: String,
+        completion: @escaping (Result<UIImage, Error>) -> Void) {
+            
+            guard let url = URL(string: url) else {
+                completion(.failure(APIError.invalidUrl))
+                return
+            }
+            let request = URLRequest(url: url)
+            
+            session.dataTask(with: request) { data, response, error in
+                let result = session.obtainResponseData(
+                    data: data, response: response, error: error)
+                switch result {
+                case .failure(let error):
+                    completion(.failure(error))
+                    return
+                case .success(let data):
+                    guard let imageData = UIImage(data: data) else {
+                        completion(.failure(APIError.convertImageFailed))
+                        return
+                    }
+                    completion(.success(imageData))
+                }
+            }.resume()
+        }
+}
