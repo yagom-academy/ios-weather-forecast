b2e058dcd96346171d84b52bb13138c45d35206a
soll4u <dasollpark@gmail.com>
2021-10-18 20:46:37 +0900

    feat: MainWeatherViewController를 rootView로 지정

--- WeatherForecast/WeatherForecast/View/MainWeatherViewController.swift
+++ WeatherForecast/WeatherForecast/View/MainWeatherViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainWeatherViewController: UIViewController {
     
     var currentWeatherViewModel = CurrentWeatherViewModel()
     var fiveDayWeatherViewModel = FiveDayWeatherViewModel()
