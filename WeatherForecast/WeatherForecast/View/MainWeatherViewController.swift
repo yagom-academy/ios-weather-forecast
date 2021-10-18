298910aa4b801209468d159931bbf6c72f5ad967
soll4u <dasollpark@gmail.com>
2021-10-18 20:49:14 +0900

    feat: MainView의 background 이미지 추가

--- WeatherForecast/WeatherForecast/View/MainWeatherViewController.swift
+++ WeatherForecast/WeatherForecast/View/MainWeatherViewController.swift
@@ -14,7 +14,20 @@ class MainWeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initBackgroundView()
+        
         currentWeatherViewModel.reload()
         fiveDayWeatherViewModel.reload()
+    }
+    
+    func initBackgroundView() {
+        let width = view.bounds.size.width
+        let height = view.bounds.size.height
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        imageView.image = UIImage(named: "seoul.jpeg")
+        imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        
+        view.addSubview(imageView)
     }
 }
