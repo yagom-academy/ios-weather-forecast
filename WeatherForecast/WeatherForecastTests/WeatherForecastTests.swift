055c1d6e5a3a49849d7d2e2f54fc6af9e1f530e3
YongHoonJJo <starctak@gmail.com>
2021-10-18 20:57:09 +0900

    refactor: 피드백 반영 - 테스트코드 given/when/then 순서 조정

--- WeatherForecast/WeatherForecastTests/WeatherForecastTests.swift
+++ WeatherForecast/WeatherForecastTests/WeatherForecastTests.swift
@@ -13,18 +13,26 @@ class WeatherForecastTests: XCTestCase {
     func test_MockLocationManager가_주입된_sut를_통해_requestLocation_메서드를_호출하면_latitude와_longitude를_가져올_수_있다() {
         // given
         let exp = XCTestExpectation(description: "MockLocationManager")
+        
         let latitude = 37.493370
         let longitude = 126.919701
-        sut = .init(locationManager: MockLocationManager(latitude: latitude, longitude: longitude))
+        
+        var resultLatitue = 0.0
+        var resultLongitude = 0.0
+        
         // when
+        sut = .init(locationManager: MockLocationManager(latitude: latitude, longitude: longitude))
         sut.requestAuthorization { coordinate in
-            // then
-            XCTAssertEqual(latitude, coordinate.latitude)
-            XCTAssertEqual(longitude, coordinate.longitude)
+            resultLatitue = coordinate.latitude
+            resultLongitude = coordinate.longitude
             exp.fulfill()
         }
         sut.requestLocation()
         wait(for: [exp], timeout: 2)
+        
+        // then
+        XCTAssertEqual(latitude, resultLatitue)
+        XCTAssertEqual(longitude, resultLongitude)
     }
     // TODOs : locationManager 의 didFailWithError 델리게이트의 내부 로직 구현 후 실패케이스 작성예정.
 }
