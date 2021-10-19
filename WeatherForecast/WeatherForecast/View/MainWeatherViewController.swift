48733a87681fae4d30979839770bbb92e22c432e
soll4u <dasollpark@gmail.com>
2021-10-19 18:28:26 +0900

    feat: MainView에 TableView 추가
    * fiveDayListViewModel의 데이터를 cell에 매핑해주고 리턴

--- WeatherForecast/WeatherForecast/View/MainWeatherViewController.swift
+++ WeatherForecast/WeatherForecast/View/MainWeatherViewController.swift
@@ -10,12 +10,14 @@ class MainWeatherViewController: UIViewController {
     
     var currentWeatherViewModel = CurrentWeatherViewModel()
     var fiveDayListViewModel = FiveDayWeatherListViewModel()
+    var weatherTableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         currentWeatherViewModel.reload()
         initBackgroundView()
+        setUpTableView()
         
     }
     
@@ -28,5 +30,38 @@ class MainWeatherViewController: UIViewController {
         imageView.contentMode = UIView.ContentMode.scaleAspectFill
         
         view.addSubview(imageView)
+    }
+    
+    private func setUpTableView() {
+        weatherTableView.delegate = self
+        weatherTableView.dataSource = self
+        view.addSubview(weatherTableView)
+        weatherTableView.register(WeatherTableViewCell.classForCoder(), forCellReuseIdentifier: "cell")
+        
+        weatherTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            weatherTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            weatherTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            weatherTableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            weatherTableView.leftAnchor.constraint(equalTo: view.leftAnchor)
+        ])
+    }
+}
+
+extension MainWeatherViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fiveDayListViewModel.numberOfRowInSection
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = weatherTableView.dequeueReusableCell(withIdentifier: "cell") as? WeatherTableViewCell else {
+            return WeatherTableViewCell()
+        }
+        let viewModel = fiveDayListViewModel.getData(at: indexPath.row)
+        cell.dateLabel.text = viewModel.dateThreeHour
+        cell.temperatureLabel.text = viewModel.temperatureThreeHour
+        cell.weatherImageView.image = viewModel.imageThreeHour
+        return cell
     }
 }
